81b6d872b748ac783d9963fec5e4524e4e2c361b
Govard Barkhatov <gbarkhatov@gmail.com>
2024-05-05 01:00:32 +0200

    rm unix function

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -29,27 +29,3 @@ export const durationTillNow = (time: string) => {
 
   return `${formattedTime} ago`;
 };
-
-export const durationTillNowUnix = (time: number) => {
-  if (!time) return "Ongoing";
-
-  const duration = intervalToDuration({
-    start: Date.now(),
-    end: new Date(time),
-  });
-  let format: (keyof Duration)[] = ["days", "hours", "minutes"];
-
-  if (!duration.days && !duration.hours && !duration.minutes) {
-    if (!duration.seconds) {
-      return "now";
-    } else {
-      format.push("seconds");
-    }
-  }
-
-  const formattedTime = formatDuration(duration, {
-    format,
-  });
-
-  return `${formattedTime} ago`;
-};
