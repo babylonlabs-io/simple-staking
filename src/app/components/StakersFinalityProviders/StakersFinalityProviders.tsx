b33c40136c051381d2dd706550da94a138909b48
Govard Barkhatov <gbarkhatov@gmail.com>
2024-05-15 19:28:11 +0200

    comment out

--- src/app/components/StakersFinalityProviders/StakersFinalityProviders.tsx
+++ src/app/components/StakersFinalityProviders/StakersFinalityProviders.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 import { FinalityProvider as FinalityProviderInterface } from "@/app/api/getFinalityProviders";
 import { FinalityProviders } from "../FinalityProviders/FinalityProviders";
-import { Stakers } from "../Stakers/Stakers";
+// import { Stakers } from "../Stakers/Stakers";
 
 interface StakersFinalityProvidersProps {
   finalityProviders: FinalityProviderInterface[] | undefined;
@@ -25,51 +25,51 @@ export const StakersFinalityProviders: React.FC<
 
   // TODO uncomment the code below this when the Stakers tab is enabled
 
-  const [activeTab, setActiveTab] = useState(0);
+  // const [activeTab, setActiveTab] = useState(0);
 
-  const tabs = ["Top Stakers", "Finality Providers"];
+  // const tabs = ["Top Stakers", "Finality Providers"];
 
-  // If wallet is not connected, show all tabs
-  // If wallet is connected, show only the active tab + tab header on mobile
-  const getActiveTabContent = (index: number) => {
-    const desktopStyles = "lg:flex lg:flex-1";
+  // // If wallet is not connected, show all tabs
+  // // If wallet is connected, show only the active tab + tab header on mobile
+  // const getActiveTabContent = (index: number) => {
+  //   const desktopStyles = "lg:flex lg:flex-1";
 
-    if (!connected) {
-      return `block ${desktopStyles}`;
-    } else {
-      return `${activeTab === index ? "block" : "hidden"} ${desktopStyles}`;
-    }
-  };
+  //   if (!connected) {
+  //     return `block ${desktopStyles}`;
+  //   } else {
+  //     return `${activeTab === index ? "block" : "hidden"} ${desktopStyles}`;
+  //   }
+  // };
 
-  return (
-    <div className="flex flex-col gap-6">
-      <div
-        role="tablist"
-        className={`${connected ? "tabs tabs-bordered lg:hidden" : "hidden"}`}
-      >
-        {tabs.map((tab, index) => (
-          <a
-            key={index}
-            role="tab"
-            // space left intentionally so the server and client side match
-            className={`tab${activeTab === index ? " tab-active" : ""}`}
-            onClick={() => setActiveTab(index)}
-          >
-            {tab}
-          </a>
-        ))}
-      </div>
-      <div className="flex flex-col gap-6 lg:flex-row">
-        <div className={getActiveTabContent(0)}>
-          <Stakers />
-        </div>
-        <div className={getActiveTabContent(1)}>
-          <FinalityProviders
-            data={finalityProviders}
-            totalActiveTVL={totalActiveTVL}
-          />
-        </div>
-      </div>
-    </div>
-  );
+  // return (
+  //   <div className="flex flex-col gap-6">
+  //     <div
+  //       role="tablist"
+  //       className={`${connected ? "tabs tabs-bordered lg:hidden" : "hidden"}`}
+  //     >
+  //       {tabs.map((tab, index) => (
+  //         <a
+  //           key={index}
+  //           role="tab"
+  //           // space left intentionally so the server and client side match
+  //           className={`tab${activeTab === index ? " tab-active" : ""}`}
+  //           onClick={() => setActiveTab(index)}
+  //         >
+  //           {tab}
+  //         </a>
+  //       ))}
+  //     </div>
+  //     <div className="flex flex-col gap-6 lg:flex-row">
+  //       <div className={getActiveTabContent(0)}>
+  //         <Stakers />
+  //       </div>
+  //       <div className={getActiveTabContent(1)}>
+  //         <FinalityProviders
+  //           data={finalityProviders}
+  //           totalActiveTVL={totalActiveTVL}
+  //         />
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 };
