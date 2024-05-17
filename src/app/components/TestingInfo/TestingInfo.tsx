0f99832192a0dd5be5ee7a62db579dc4cd325add
Govard Barkhatov <gbarkhatov@gmail.com>
2024-05-17 16:10:57 +0200

    testing app banner

--- src/app/components/TestingInfo/TestingInfo.tsx
+++ src/app/components/TestingInfo/TestingInfo.tsx
@@ -5,15 +5,16 @@ interface TestingInfoProps {}
 export const TestingInfo: React.FC<TestingInfoProps> = () => {
   return (
     <div className="flex w-full justify-center">
-      <div className="card flex w-full flex-col items-center gap-1 bg-base-200 p-3 text-sm md:w-auto md:flex-row">
-        <div className="flex items-center gap-1 md:gap-2">
+      <div className="card flex w-full flex-col items-center gap-1 bg-base-200 p-3 text-sm lg:w-auto lg:flex-row lg:text-xs">
+        <div className="flex items-center gap-1 lg:gap-2">
           <RiErrorWarningLine size={20} />
-          <p>
-            <strong>Testing app</strong>
+          <p className="whitespace-nowrap text-center">
+            <strong>This is a testing Dapp.</strong>
           </p>
         </div>
         <p className="text-center lg:text-left">
-          No financial service. No real tokens. Use it at your own discretion.
+          Please do your due dilligence. Tokens are for testing only & do not
+          carry any monetary value.
         </p>
       </div>
     </div>
