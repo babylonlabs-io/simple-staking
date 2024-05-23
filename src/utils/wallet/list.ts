f8550697529cb5ac89f07640e8872d0fc9558a6d
Govard Barkhatov <gbarkhatov@gmail.com>
2024-05-23 16:21:07 +0200

    qr wallets support

--- src/utils/wallet/list.ts
+++ src/utils/wallet/list.ts
@@ -1,7 +1,16 @@
 import { OKXWallet, okxProvider } from "./providers/okx_wallet";
 import okxIcon from "./icons/okx.svg";
 
-export const walletList = [
+interface IntegratedWallet {
+  name: string;
+  icon: string;
+  wallet: any;
+  linkToDocs: string;
+  provider?: string;
+  isQRWallet?: boolean;
+}
+
+export const walletList: IntegratedWallet[] = [
   {
     name: "OKX",
     icon: okxIcon,
