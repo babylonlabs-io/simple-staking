55ef5517f811e00d60b9967498a0fa72cff515b3
Govard Barkhatov <gbarkhatov@gmail.com>
2024-05-30 15:51:04 +0200

    change position

--- src/app/components/Delegations/Delegation.tsx
+++ src/app/components/Delegations/Delegation.tsx
@@ -118,7 +118,7 @@ export const Delegation: React.FC<DelegationProps> = ({
       className={`card relative border bg-base-300 p-4 text-sm dark:bg-base-200 ${isOverflow ? "border-primary" : "dark:border-0"}`}
     >
       {isOverflow && (
-        <div className="absolute -top-3 right-1/2 flex translate-x-1/2 items-center gap-1 rounded-md bg-primary px-2 py-1 text-xs text-white lg:right-2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-0">
+        <div className="absolute -top-1 right-1/2 flex translate-x-1/2 items-center gap-1 rounded-md bg-primary px-2 py-1 text-xs text-white lg:right-2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-0">
           <IoIosWarning size={16} />
           <p>overflow</p>
         </div>
