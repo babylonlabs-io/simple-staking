20d6bbf235df34f4d4d37a8ff1e3bb034b2915cd
wjrjerome <wjrjerome@babylonchain.io>
2024-06-07 18:59:22 +1000

    make the staking window uppercase for word window

--- src/app/components/Stats/Stats.tsx
+++ src/app/components/Stats/Stats.tsx
@@ -37,7 +37,7 @@ const buildStakingCapSection = (
       };
     } else if (verionedParam?.stakingCapHeight) {
       return {
-        title: "Staking window",
+        title: "Staking Window",
         value: `height ${activationHeight} - ${stakingCapHeight} (current: ${btcHeight})`,
       };
     }
