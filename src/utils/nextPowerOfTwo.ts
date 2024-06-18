a081efa06a14b250320ac97142da77024dcfeddf
Govard Barkhatov <gbarkhatov@gmail.com>
2024-06-18 14:59:06 +0200

    calc +1

--- src/utils/nextPowerOfTwo.ts
+++ src/utils/nextPowerOfTwo.ts
@@ -2,5 +2,5 @@ export const nextPowerOfTwo = (x: number) => {
   if (x <= 0) return 2;
   if (x === 1) return 4;
 
-  return Math.pow(2, Math.ceil(Math.log2(x))) * 2;
+  return Math.pow(2, Math.ceil(Math.log2(x)) + 1);
 };
