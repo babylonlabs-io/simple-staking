af1e732c78c2adb61a143f3ddd8754eb4c30aa1c
wjrjerome <wjrjerome@babylonchain.io>
2024-06-18 19:35:14 +1000

    add unit test for the blocksToDisplayTime

---
+++ tests/utils/blocksToDisplayTime.test.ts
@@ -0,0 +1,27 @@
+import { blocksToDisplayTime } from "@/utils/blocksToDisplayTime";
+
+describe("blocksToDisplayTime", () => {
+  it("should throw error if block is 0", () => {
+    expect(() => blocksToDisplayTime(0)).toThrow("No blocks provided");
+  });
+
+  it("should convert 1 block to 1 day", () => {
+    expect(blocksToDisplayTime(1)).toBe("1 day");
+  });
+
+  it("should convert 200 blocks to 2 days", () => {
+    expect(blocksToDisplayTime(200)).toBe("2 days");
+  });
+
+  it("should convert 900 blocks to 7 days", () => {
+    expect(blocksToDisplayTime(900)).toBe("7 days");
+  });
+
+  it("should convert 30000 blocks to 30 weeks", () => {
+    expect(blocksToDisplayTime(30000)).toBe("30 weeks");
+  });
+
+  it("should convert 4320 blocks to 5 weeks", () => {
+    expect(blocksToDisplayTime(4320)).toBe("5 weeks");
+  });
+});
