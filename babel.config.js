6346dd6e0d0c837f669401c046b9e96bf9bb8ab7
wjrjerome <wjrjerome@babylonchain.io>
2024-06-18 14:15:25 +1000

    chore: remove the unused console.log

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,5 @@
 module.exports = function (api) {
   const isTest = api.env("test") || process.env.JEST_ENV === "true";
-  console.log(isTest);
   if (!isTest) return {}; // Don't need to do anything for non-test environments
   const presets = [
     ["@babel/preset-env", { targets: { node: "current" } }],
