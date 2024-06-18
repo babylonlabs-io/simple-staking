8efcd70f10d2a570d781c511b6cb0c7af68ae381
Govard Barkhatov <gbarkhatov@gmail.com>
2024-06-18 15:03:32 +0200

    LEAST_MAX_FEE_RATE

--- src/utils/getFeeRateFromMempool.ts
+++ src/utils/getFeeRateFromMempool.ts
@@ -5,12 +5,12 @@ import { Fees } from "./wallet/wallet_provider";
 export const getFeeRateFromMempool = (mempoolFeeRates?: Fees) => {
   if (mempoolFeeRates) {
     // The maximum fee rate is at least 128 sat/vB
-    const MAX_FEE_RATE = 128;
+    const LEAST_MAX_FEE_RATE = 128;
     return {
       minFeeRate: mempoolFeeRates.hourFee,
       defaultFeeRate: mempoolFeeRates.fastestFee,
       maxFeeRate: Math.max(
-        MAX_FEE_RATE,
+        LEAST_MAX_FEE_RATE,
         nextPowerOfTwo(mempoolFeeRates.fastestFee),
       ),
     };
