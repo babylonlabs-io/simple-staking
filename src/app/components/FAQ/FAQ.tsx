6b3a37016ac26c8c6c0cb015998032c60f204a59
wjrjerome <wjrjerome@babylonchain.io>
2024-06-18 20:38:27 +1000

    fix: update FAQ for overflow and use coinName instead of hardcoded sBTC

--- src/app/components/FAQ/FAQ.tsx
+++ src/app/components/FAQ/FAQ.tsx
@@ -1,14 +1,17 @@
+import { getNetworkConfig } from "@/config/network.config";
+
 import { Section } from "./Section";
 import { questions } from "./data/questions";
 
 interface FAQProps {}
 
 export const FAQ: React.FC<FAQProps> = () => {
+  const { coinName } = getNetworkConfig();
   return (
     <div className="container mx-auto flex flex-col gap-2 p-6">
       <h3 className="mb-4 font-bold">FAQ</h3>
       <div className="flex flex-col gap-4">
-        {questions.map((question) => (
+        {questions(coinName).map((question) => (
           <Section
             key={question.title}
             title={question.title}
--- src/app/components/FAQ/data/questions.ts
+++ src/app/components/FAQ/data/questions.ts
@@ -1,32 +1,32 @@
-export const questions = [
+export const questions = (coinName: string) => [
   {
     title: "What is Babylon?",
     content: `<p>Babylon is a suite of security-sharing protocols that bring Bitcoin\’s unparalleled security to the decentralized world. The latest protocol, Bitcoin Staking, enables Bitcoin holders to stake their Bitcoin to provide crypto-economic security to PoS (proof-of-stake) systems in a trustless and self-custodial way.</p>`,
   },
   {
     title: "How does Bitcoin Staking Work?",
-    content: `<p>Signet Bitcoin holders lock their Signet Bitcoin using the trustless and self-custodial Bitcoin Staking script for a predetermined time (timelock) in exchange for voting power in an underlying PoS protocol. In return, Bitcoin holders will earn PoS staking rewards.</p><br />
-      <p>Finality providers perform the voting. A Signet Bitcoin staker can create a finality provider by itself and self-delegate or delegate its voting power to a third-party finality provider.</p><br />
-      <p>If a finality provider attacks the PoS system, the Signet Bitcoins behind the voting powers delegated to it will be subject to protocol slashing. This deters Signet Bitcoin stakers and finality providers from attacking the PoS system.</p>
+    content: `<p>${coinName} holders lock their ${coinName} using the trustless and self-custodial Bitcoin Staking script for a predetermined time (timelock) in exchange for voting power in an underlying PoS protocol. In return, Bitcoin holders will earn PoS staking rewards.</p><br />
+      <p>Finality providers perform the voting. A ${coinName} staker can create a finality provider by itself and self-delegate or delegate its voting power to a third-party finality provider.</p><br />
+      <p>If a finality provider attacks the PoS system, the ${coinName}s behind the voting powers delegated to it will be subject to protocol slashing. This deters ${coinName} stakers and finality providers from attacking the PoS system.</p>
       `,
   },
   {
     title: "What is the goal of this testnet?",
-    content: `<p>The goal of this testnet is to ensure the security of the staked Bitcoins by testing the user's interaction with the Signet BTC network. This will be a lock-only network without any PoS chain operating, meaning that the only participants of this testnet will be finality providers and Signet Bitcoin stakers.</p>`,
+    content: `<p>The goal of this testnet is to ensure the security of the staked Bitcoins by testing the user's interaction with the ${coinName} network. This will be a lock-only network without any PoS chain operating, meaning that the only participants of this testnet will be finality providers and ${coinName} stakers.</p>`,
   },
   {
     title: "What does this staking dApp allow me to do?",
-    content: `<p>The staking dApp allows Signet Bitcoin holders to stake their Signet Bitcoin and delegate their voting power to a finality provider they select. Stakers can view their past staking history and on-demand unlock their stake for early withdrawal.</p>`,
+    content: `<p>The staking dApp allows ${coinName} holders to stake their ${coinName} and delegate their voting power to a finality provider they select. Stakers can view their past staking history and on-demand unlock their stake for early withdrawal.</p>`,
   },
   {
-    title: "Does my Signet Bitcoin leave my wallet once staked?",
-    content: `<p>Yes, it leaves your wallet. Your wallet will not show it as your available balance because it is locked. However, it is not sent to any third party. It is locked in a self-custodial contract you control. This means that any subsequent movement of the Signet Bitcoin will need your approval, and you are the only one who can unbond the stake and withdraw.</p>`,
+    title: `Does my ${coinName} leave my wallet once staked?`,
+    content: `<p>Yes, it leaves your wallet. Your wallet will not show it as your available balance because it is locked. However, it is not sent to any third party. It is locked in a self-custodial contract you control. This means that any subsequent movement of the ${coinName} will need your approval, and you are the only one who can unbond the stake and withdraw.</p>`,
   },
   {
-    title: "Is my Signet Bitcoin Safe? Could I get slashed?",
-    content: `<p>In this testnet, you are not required to sign any PoS slashing-related authorizations. Thus, in theory, the Signet Bitcoin in your self-custodial contract cannot be slashed due to the absence of your authorization.</p><br />
+    title: `Is my ${coinName} Safe? Could I get slashed?`,
+    content: `<p>In this testnet, you are not required to sign any PoS slashing-related authorizations. Thus, in theory, the ${coinName} in your self-custodial contract cannot be slashed due to the absence of your authorization.</p><br />
 
-    <p>However, there are still risks associated with your Signet Bitcoin:</p><br />
+    <p>However, there are still risks associated with your ${coinName}:</p><br />
     
     <ol>
       <li>
@@ -36,14 +36,19 @@ export const questions = [
       <br />
       <li>
         2. System reliability<br />
-        The Bitcoin staking system may be slow, unavailable, or compromised, which may cause the staking service to be unavailable or compromised, potentially leading to Signet Bitcoin not being unbindable or not withdrawable.
+        The Bitcoin staking system may be slow, unavailable, or compromised, which may cause the staking service to be unavailable or compromised, potentially leading to ${coinName} not being unbindable or not withdrawable.
       </li>
     </ol>
     `,
   },
   {
     title: "How long will it take for my stake to become active?",
-    content: `<p>A stake’s status demonstrates the current stage of the staking process. All stake starts in a Pending state which denotes that the Signet Bitcoin Staking transaction does not yet have sufficient Signet BTC block confirmations. As soon as it receives 10 Signet Bitcoin block confirmations, the status transitions to <i>Overflow</i> or <i>Active</i>. A stake is <i>Overflow</i> if the system has already accumulated the maximum amount of Signet Bitcoin it can accept. Otherwise, the stake is <i>Active</i>. <i>Overflow</i> stake should be on-demand unbonded and withdrawn.</p>`,
+    content: `<p>A stake’s status demonstrates the current stage of the staking process. All stake starts in a Pending state which denotes that the ${coinName} Staking transaction does not yet have sufficient ${coinName} block confirmations. As soon as it receives 10 ${coinName} block confirmations, the status transitions to <i>Overflow</i> or <i>Active</i>. </p><br />
+    
+    <p>In an amount-based cap, A stake is <i>Overflow</i> if the system has already accumulated the maximum amount of ${coinName} it can accept.</p><br />
+    <p>In a time-based cap, where there is a starting block height and ending block height, a stake is <i>overflow</i> if it is included in a ${coinName} block that is newer than the ending block.</p><br /> 
+    <p>Otherwise, the stake is <i>Active</i>.</p><br /> 
+    <p><i>Overflow</i> stake should be on-demand unbonded and withdrawn.</p>`,
   },
   {
     title: "Do I get rewards for staking?",
@@ -51,7 +56,7 @@ export const questions = [
   },
   {
     title: "Are there any other ways to stake?",
-    content: `<p>Hands-on stakers can operate the <a href="https://github.com/babylonchain/btc-staker/blob/dev/docs/create-phase1-staking.md" target="_blank" rel="noopener noreferrer" class="text-primary"><u>btc-staker CLI program</u></a> that allows for the creation of Signet Bitcoin staking transactions from the CLI.</p>
+    content: `<p>Hands-on stakers can operate the <a href="https://github.com/babylonchain/btc-staker/blob/dev/docs/create-phase1-staking.md" target="_blank" rel="noopener noreferrer" class="text-primary"><u>btc-staker CLI program</u></a> that allows for the creation of ${coinName} staking transactions from the CLI.</p>
     `,
   },
 ];
