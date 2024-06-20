3a91657b4f71f991293e61c07c31935722087731
Govard Barkhatov <gbarkhatov@gmail.com>
2024-06-20 11:36:18 +0200

    rm output index

--- src/utils/delegations/signWithdrawalTx.ts
+++ src/utils/delegations/signWithdrawalTx.ts
@@ -86,7 +86,6 @@ export const signWithdrawalTx = async (
       address,
       btcWalletNetwork,
       fees.fastestFee,
-      0,
     );
   } else {
     // Withdraw funds from a staking transaction in which the timelock naturally expired
