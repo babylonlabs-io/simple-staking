206aae1b7605c4f9b6cc961fb7c402a16ae32aaa
Vladyslav Pobihailo <v.pobihailo@everstake.one>
2024-07-04 13:07:40 +0300

    faq && styles setup

--- src/app/components/FAQ/FAQ.tsx
+++ src/app/components/FAQ/FAQ.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { getNetworkConfig } from "@/config/network.config";
 
 import { Section } from "./Section";
@@ -7,15 +9,25 @@ interface FAQProps {}
 
 export const FAQ: React.FC<FAQProps> = () => {
   const { coinName } = getNetworkConfig();
+
+  const [openSection, setOpenSection] = useState<string | null>(
+    questions(coinName).length > 0 ? questions(coinName)[0].title : null,
+  );
+
+  const handleSectionClick = (title: string) => {
+    setOpenSection(openSection === title ? null : title);
+  };
   return (
     <div className="container mx-auto flex flex-col gap-2 p-6">
-      <h3 className="mb-4 font-bold">FAQ</h3>
-      <div className="flex flex-col gap-4">
+      <h3 className="mb-4 font-bold text-[88px] text-bold">FAQ</h3>
+      <div className="flex flex-col">
         {questions(coinName).map((question) => (
           <Section
             key={question.title}
             title={question.title}
             content={question.content}
+            isOpen={openSection === question.title}
+            onClick={() => handleSectionClick(question.title)}
           />
         ))}
       </div>
--- src/app/components/FAQ/Section.tsx
+++ src/app/components/FAQ/Section.tsx
@@ -1,35 +1,65 @@
 import { AnimatePresence, motion } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FaMinus, FaPlus } from "react-icons/fa";
 
 interface SectionProps {
   title: string;
   content: string;
+  isOpen: boolean;
+  onClick: () => void;
 }
 
-export const Section: React.FC<SectionProps> = ({ title, content }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
+export const Section: React.FC<SectionProps> = ({
+  title,
+  content,
+  isOpen,
+  onClick,
+}) => {
+  const contentRef = useRef<HTMLDivElement>(null);
+  const [maxHeight, setMaxHeight] = useState("0px");
+  useEffect(() => {
+    if (isOpen) {
+      const contentHeight = contentRef.current
+        ? `${contentRef.current.scrollHeight + 28}px`
+        : "0px"; // 28 = parent padding
+      setMaxHeight(contentHeight);
+    } else {
+      setMaxHeight("0px");
+    }
+  }, [isOpen]);
   return (
     <div
-      className="card cursor-pointer border bg-base-300 p-4 dark:border-0"
-      onClick={() => setIsOpen(!isOpen)}
+      className={`card cursor-pointer p-8 rounded-none border transition-colors ${isOpen ? "border-es-accent" : "border-[#5b5b5b]"}`}
+      onClick={() => onClick()}
     >
       <div className="flex items-center justify-between">
-        <h4 className="font-bold">{title}</h4>
-        <button className="btn btn-square btn-sm border border-neutral-content bg-transparent">
-          {isOpen ? <FaMinus /> : <FaPlus />}
+        <h4
+          className={`font-bold text-lg uppercase ${isOpen ? "text-es-accent" : ""}`}
+        >
+          {title}
+        </h4>
+        <button
+          className={`btn btn-square btn-sm bg-transparent border-0 ${isOpen ? "" : ""}`}
+        >
+          {isOpen ? <FaMinus fill="#F5BE37" /> : <FaPlus />}
         </button>
       </div>
-      <AnimatePresence>
+      <AnimatePresence initial={false}>
         {isOpen && (
-          <motion.p
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            className="mt-4"
-            dangerouslySetInnerHTML={{ __html: content }}
-          />
+          <motion.div
+            initial={{ maxHeight: "0px" }}
+            animate={{ maxHeight }}
+            exit={{ maxHeight: "0px" }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className="overflow-hidden"
+          >
+            <div ref={contentRef}>
+              <div
+                className="mt-4 text-sm"
+                dangerouslySetInnerHTML={{ __html: content }}
+              />
+            </div>
+          </motion.div>
         )}
       </AnimatePresence>
     </div>
