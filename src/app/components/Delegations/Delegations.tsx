019a070d13d0871ea0ff610766e20391d81d44e0
wjrjerome <wjrjerome@babylonchain.io>
2024-07-08 23:20:11 +1000

    chore: add fee wanring to unbonding and withdraw tx

--- src/app/components/Delegations/Delegations.tsx
+++ src/app/components/Delegations/Delegations.tsx
@@ -129,7 +129,6 @@ export const Delegations: React.FC<DelegationsProps> = ({
       const { delegation } = await signWithdrawalTx(
         id,
         delegationsAPI,
-        globalParamsVersion,
         publicKeyNoCoord,
         btcWalletNetwork,
         signPsbtTx,
@@ -284,6 +283,7 @@ export const Delegations: React.FC<DelegationsProps> = ({
       {modalMode && txID && (
         <UnbondWithdrawModal
           unbondingTimeBlocks={globalParamsVersion.unbondingTime}
+          unbondingFeeSat={globalParamsVersion.unbondingFeeSat}
           open={modalOpen}
           onClose={() => setModalOpen(false)}
           onProceed={() => {
