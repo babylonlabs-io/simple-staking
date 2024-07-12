3266b639b5791a4f18fbba93ddb129579178f5ac
wjrjerome <wjrjerome@babylonchain.io>
2024-07-12 15:30:32 +1000

    fix: remove testnet wordings for upcoming release

---
+++ tests/config/index.test.ts
@@ -0,0 +1,21 @@
+import { shouldDisplayTestingMsg } from "@/config";
+
+describe("shouldDisplayTestingMsg", () => {
+  afterEach(() => {
+    delete process.env.NEXT_PUBLIC_DISPLAY_TESTING_MESSAGES;
+  });
+
+  it("should return true if NEXT_PUBLIC_DISPLAY_TESTING_MESSAGES is not set", () => {
+    expect(shouldDisplayTestingMsg()).toBe(true);
+  });
+
+  it('should return true if NEXT_PUBLIC_DISPLAY_TESTING_MESSAGES is "true"', () => {
+    process.env.NEXT_PUBLIC_DISPLAY_TESTING_MESSAGES = "true";
+    expect(shouldDisplayTestingMsg()).toBe(true);
+  });
+
+  it('should return false if NEXT_PUBLIC_DISPLAY_TESTING_MESSAGES is "false"', () => {
+    process.env.NEXT_PUBLIC_DISPLAY_TESTING_MESSAGES = "false";
+    expect(shouldDisplayTestingMsg()).toBe(false);
+  });
+});
