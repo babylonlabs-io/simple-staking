5ba22475b0f9d785037264767c8aeb8c898163b8
jeremy-babylonchain <jeremy.tsang@babylonchain.io>
2024-07-15 14:52:08 +0800

    fix networkBadge height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -345,7 +345,7 @@ const Home: React.FC<HomeProps> = () => {
     <main
       className={`relative h-full min-h-svh w-full ${network === Network.MAINNET ? "main-app-mainnet" : "main-app-testnet"}`}
     >
-      <NetworkBadge />
+      <NetworkBadge isWalletConnected={!!btcWallet} />
       <Header
         onConnect={handleConnectModal}
         onDisconnect={handleDisconnectBTC}
