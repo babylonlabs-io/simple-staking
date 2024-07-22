b7d3c785d08202c8e60b4400d784bbb460a2bcf0
wjrjerome <wjrjerome@babylonchain.io>
2024-07-22 15:15:51 +1000

    send btc address when verifying the utoxs

--- src/app/api/postFilterOrdinals.ts
+++ src/app/api/postFilterOrdinals.ts
@@ -2,13 +2,36 @@ import { UTXO } from "@/utils/wallet/wallet_provider";
 
 import { apiWrapper } from "./apiWrapper";
 
-export const postFilterOrdinals = async (utxos: UTXO[]) => {
-  const response = await apiWrapper(
+interface UtxoInfo {
+  txid: string;
+  inscription: boolean;
+}
+
+export const postFilterOrdinals = async (
+  utxos: UTXO[],
+  address: string,
+): Promise<UTXO[]> => {
+  const { data }: { data: { data: UtxoInfo[] } } = await apiWrapper(
     "POST",
     "/v1/ordinals/verify-utxos",
     "Error filtering ordinals",
-    utxos,
+    {
+      address,
+      utxos: utxos.map((utxo) => ({
+        txid: utxo.txid,
+        vout: utxo.vout,
+      })),
+    },
+  );
+  // turn the data into map with key of the txid
+  const utxoInfoMap = data.data.reduce(
+    (acc: Record<string, boolean>, utxo: UtxoInfo) => {
+      acc[utxo.txid] = utxo.inscription;
+      return acc;
+    },
+    {},
   );
 
-  return response.data;
+  // filter out the ordinals
+  return utxos.filter((utxo) => !utxoInfoMap[utxo.txid]);
 };
