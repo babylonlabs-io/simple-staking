15694ed9818663f4bd25d7c2c1444c278a7145b4
Govard Barkhatov <gbarkhatov@gmail.com>
2024-07-22 21:30:36 +0200

    post filter + chunks

--- src/app/api/postFilterOrdinals.ts
+++ src/app/api/postFilterOrdinals.ts
@@ -1,3 +1,4 @@
+import { chunkArray } from "@/utils/chunkArray";
 import { UTXO } from "@/utils/wallet/wallet_provider";
 
 import { apiWrapper } from "./apiWrapper";
@@ -13,24 +14,32 @@ export const postFilterOrdinals = async (
   address: string,
 ): Promise<UTXO[]> => {
   const TIMEOUT_DURATION = 2000; // 2 seconds
+  const BATCH_SIZE = 30;
 
   try {
-    const { data }: { data: { data: UtxoInfo[] } } = await apiWrapper(
-      "POST",
-      "/v1/ordinals/verify-utxos",
-      "Error filtering ordinals",
-      {
-        address,
-        utxos: utxos.map((utxo) => ({
-          txid: utxo.txid,
-          vout: utxo.vout,
-        })),
-      },
-      TIMEOUT_DURATION,
+    const utxoChunks = chunkArray(utxos, BATCH_SIZE);
+    const responses = await Promise.all(
+      utxoChunks.map((chunk) =>
+        apiWrapper(
+          "POST",
+          "/v1/ordinals/verify-utxos",
+          "Error filtering ordinals",
+          {
+            address,
+            utxos: chunk.map((utxo) => ({
+              txid: utxo.txid,
+              vout: utxo.vout,
+            })),
+          },
+          TIMEOUT_DURATION,
+        ),
+      ),
     );
 
+    const allUtxoInfo = responses.flatMap((response) => response.data.data);
+
     // turn the data into map with key of the `txid:vout`
-    const utxoInfoMap = data.data.reduce(
+    const utxoInfoMap = allUtxoInfo.reduce(
       (acc: Record<string, boolean>, u: UtxoInfo) => {
         acc[getUTXOIdentifier(u)] = u.inscription;
         return acc;
