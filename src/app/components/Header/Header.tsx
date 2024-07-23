52716d8c86fce9ecf56023f7c780f5335cfa8fbe
Govard Barkhatov <gbarkhatov@gmail.com>
2024-07-23 20:27:30 +0200

    connected small revert

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -41,7 +41,7 @@ export const Header: React.FC<HeaderProps> = ({
         >
           <ConnectedSmall
             address={address}
-            balanceSat={balanceSat}
+            btcWalletBalanceSat={btcWalletBalanceSat}
             onDisconnect={onDisconnect}
           />
         </div>
