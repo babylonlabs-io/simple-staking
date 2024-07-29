1b81655832976c04aecabf1d6fc6df17050cd576
Jeremy <168515712+jeremy-babylonchain@users.noreply.github.com>
2024-07-29 14:44:32 +0800

    fix balance view (#384)
    * fix balance view
    
    * version bump and fix potentional error

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -21,29 +21,31 @@ export const Header: React.FC<HeaderProps> = ({
   return (
     <nav>
       <div className="bg-base-300 shadow-sm">
-        <div className="container mx-auto flex w-full items-center justify-between gap-4 p-6 pb-4 md:pb-6">
-          <Logo />
-          <div className="flex flex-1">
-            <div className="hidden flex-1 xl:flex">
-              <TestingInfo />
+        <div className="container mx-auto flex-col">
+          <div className="w-full flex items-center justify-between gap-4 p-6 pb-4 md:pb-6">
+            <Logo />
+            <div className="flex flex-1">
+              <div className="hidden flex-1 xl:flex">
+                <TestingInfo />
+              </div>
             </div>
+            <ConnectSmall
+              onConnect={onConnect}
+              address={address}
+              btcWalletBalanceSat={btcWalletBalanceSat}
+              onDisconnect={onDisconnect}
+            />
+            <ThemeToggle />
           </div>
-          <ConnectSmall
-            onConnect={onConnect}
-            address={address}
-            btcWalletBalanceSat={btcWalletBalanceSat}
-            onDisconnect={onDisconnect}
-          />
-          <ThemeToggle />
-        </div>
-        <div
-          className={`${address && "justify-end p-6 pt-0"}container mx-auto flex w-full items-center gap-4 md:hidden md:p-0`}
-        >
-          <ConnectedSmall
-            address={address}
-            btcWalletBalanceSat={btcWalletBalanceSat}
-            onDisconnect={onDisconnect}
-          />
+          <div
+            className={`container mx-auto flex w-full items-center gap-4 md:hidden md:p-0 ${address ? "justify-end p-6 pt-0" : ""}`}
+          >
+            <ConnectedSmall
+              address={address}
+              btcWalletBalanceSat={btcWalletBalanceSat}
+              onDisconnect={onDisconnect}
+            />
+          </div>
         </div>
       </div>
       <div className="container mx-auto flex w-full items-center p-6 pb-0 xl:hidden">
