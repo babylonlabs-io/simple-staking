e7857a0ec476d517dbccd7416cb345bae95aee8f
jeremy-babylonlabs <jeremy.t@babylonlabs.io>
2024-08-01 00:23:22 +0800

    remove sorting

--- src/app/components/Staking/FinalityProviders/FinalityProviders.tsx
+++ src/app/components/Staking/FinalityProviders/FinalityProviders.tsx
@@ -1,7 +1,3 @@
-import { useWindowSize } from "@uidotdev/usehooks";
-import { useEffect } from "react";
-import { BsSortDown } from "react-icons/bs";
-import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 import {
@@ -9,17 +5,9 @@ import {
   LoadingView,
 } from "@/app/components/Loading/Loading";
 import { useFinalityProvidersData } from "@/app/hooks/finalityProviders/useFinalityProvidersData";
-import { useFinalityProvidersSort } from "@/app/hooks/finalityProviders/useFinalityProvidersSort";
-import { useMobileSortModal } from "@/app/hooks/finalityProviders/useMobileSortModal";
-import {
-  FinalityProvidersProps,
-  FinalityProvidersSortButtonProps,
-  SortField,
-} from "@/app/types/finalityProviders";
+import { FinalityProvidersProps } from "@/app/types/finalityProviders";
 import { getNetworkConfig } from "@/config/network.config";
 import { Network } from "@/utils/wallet/wallet_provider";
-
-import { FinalityProviderMobileSortModal } from "../../Modals/FinalityProviderMobileSortModal";
 
 import { FinalityProvider } from "./FinalityProvider";
 import { FinalityProviderSearch } from "./FinalityProviderSearch";
@@ -30,57 +18,8 @@ export const FinalityProviders: React.FC<FinalityProvidersProps> = ({
   onFinalityProviderChange,
   queryMeta,
 }) => {
-  const { width } = useWindowSize();
-  const isMobile = typeof width === "number" ? width < 1000 : false;
-
-  const { handleSearch, sortedProviders, setSortField, setSortDirection } =
+  const { handleSearch, filteredProviders } =
     useFinalityProvidersData(finalityProviders);
-
-  const { visualSortField, visualSortDirection, handleSort, handleMobileSort } =
-    useFinalityProvidersSort(setSortField, setSortDirection);
-
-  const {
-    isSortModalOpen,
-    toggleSortModal,
-    activeOrder,
-    setActiveOrder,
-    selectedCriterion,
-    setSelectedCriterion,
-    setIsSortModalOpen,
-  } = useMobileSortModal();
-
-  useEffect(() => {
-    if (!isMobile && isSortModalOpen) {
-      setIsSortModalOpen(false);
-    }
-  }, [isMobile, isSortModalOpen, setIsSortModalOpen]);
-
-  const SortIndicator = ({ field }: { field: SortField }) => {
-    const isSelected = visualSortField === field;
-    const Icon =
-      isSelected && visualSortDirection === "asc"
-        ? MdKeyboardArrowUp
-        : MdKeyboardArrowDown;
-    return (
-      <span
-        className={`inline-flex items-center justify-center px-2 ${isSelected ? "text-primary" : "text-gray-400"}`}
-      >
-        <Icon size={20} />
-      </span>
-    );
-  };
-
-  const SortButton: React.FC<FinalityProvidersSortButtonProps> = ({
-    field,
-    label,
-    onSort,
-  }) => {
-    return (
-      <button onClick={() => onSort(field)} className="flex items-center">
-        {label} <SortIndicator field={field} />
-      </button>
-    );
-  };
 
   if (!finalityProviders || finalityProviders.length === 0) {
     return <LoadingView />;
@@ -107,48 +46,26 @@ export const FinalityProviders: React.FC<FinalityProvidersProps> = ({
       </p>
       <div className="flex gap-3">
         <FinalityProviderSearch onSearch={handleSearch} />
-        {isMobile && (
-          <button
-            onClick={toggleSortModal}
-            className="btn btn-outline btn-xs text-sm mt-0 font-normal rounded-xl border-gray-400 p-2 h-auto lg:hidden"
-          >
-            <BsSortDown className="text-xl" />
-          </button>
-        )}
       </div>
-      {!isMobile && (
-        <div className="hidden gap-2 px-4 lg:grid lg:grid-cols-stakingFinalityProvidersDesktop">
-          <SortButton
-            field="moniker"
-            label="Finality Provider"
-            onSort={handleSort}
-          />
-          <SortButton field="btcPk" label="BTC PK" onSort={handleSort} />
-          <SortButton
-            field="stakeSat"
-            label="Total Delegation"
-            onSort={handleSort}
-          />
-          <SortButton
-            field="commission"
-            label="Commission"
-            onSort={handleSort}
-          />
-        </div>
-      )}
+      <div className="hidden gap-2 px-4 lg:grid lg:grid-cols-stakingFinalityProvidersDesktop">
+        <p>Finality Provider</p>
+        <p>BTC PK</p>
+        <p>Total Delegation</p>
+        <p>Commission</p>
+      </div>
       <div
         id="finality-providers"
         className="no-scrollbar max-h-[21rem] overflow-y-auto"
       >
         <InfiniteScroll
           className="flex flex-col gap-4"
-          dataLength={sortedProviders.length}
+          dataLength={filteredProviders?.length || 0}
           next={queryMeta.next}
           hasMore={queryMeta.hasMore}
           loader={queryMeta.isFetchingMore ? <LoadingTableList /> : null}
           scrollableTarget="finality-providers"
         >
-          {sortedProviders.map((fp) => (
+          {filteredProviders?.map((fp) => (
             <FinalityProvider
               key={fp.btcPk}
               moniker={fp.description?.moniker}
@@ -164,17 +81,6 @@ export const FinalityProviders: React.FC<FinalityProvidersProps> = ({
           ))}
         </InfiniteScroll>
       </div>
-      {isSortModalOpen && (
-        <FinalityProviderMobileSortModal
-          open={isSortModalOpen}
-          onClose={() => setIsSortModalOpen(false)}
-          handleMobileSort={handleMobileSort}
-          activeOrder={activeOrder}
-          setActiveOrder={setActiveOrder}
-          selectedCriterion={selectedCriterion}
-          setSelectedCriterion={setSelectedCriterion}
-        />
-      )}
     </>
   );
 };
--- src/app/hooks/finalityProviders/useFinalityProvidersData.ts
+++ src/app/hooks/finalityProviders/useFinalityProvidersData.ts
@@ -1,17 +1,11 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
-import {
-  FinalityProvider as FinalityProviderInterface,
-  SortDirection,
-  SortField,
-} from "@/app/types/finalityProviders";
+import { FinalityProvider as FinalityProviderInterface } from "@/app/types/finalityProviders";
 
 export const useFinalityProvidersData = (
   initialProviders: FinalityProviderInterface[] | undefined,
 ) => {
   const [filteredProviders, setFilteredProviders] = useState(initialProviders);
-  const [sortField, setSortField] = useState<SortField>("stakeSat");
-  const [sortDirection, setSortDirection] = useState<SortDirection>("desc");
 
   useEffect(() => {
     setFilteredProviders(initialProviders);
@@ -31,61 +25,9 @@ export const useFinalityProvidersData = (
     [initialProviders],
   );
 
-  const sortFinalityProviders = useCallback(
-    (
-      providers: FinalityProviderInterface[] | undefined,
-      field: SortField,
-      direction: SortDirection,
-    ) => {
-      if (!providers || providers.length === 0) {
-        return [];
-      }
-
-      return [...providers].sort((a, b) => {
-        const aValue = getSortValue(a, field);
-        const bValue = getSortValue(b, field);
-        return direction === "asc"
-          ? aValue > bValue
-            ? 1
-            : -1
-          : bValue > aValue
-            ? 1
-            : -1;
-      });
-    },
-    [],
-  );
-
-  const sortedProviders = useMemo(
-    () => sortFinalityProviders(filteredProviders, sortField, sortDirection),
-    [filteredProviders, sortField, sortDirection, sortFinalityProviders],
-  );
-
   return {
     filteredProviders,
     setFilteredProviders,
     handleSearch,
-    sortedProviders,
-    setSortField,
-    setSortDirection,
-    sortFinalityProviders,
   };
-};
-
-const getSortValue = (
-  provider: FinalityProviderInterface,
-  field: SortField,
-) => {
-  switch (field) {
-    case "moniker":
-      return provider.description?.moniker || "";
-    case "btcPk":
-      return provider.btcPk;
-    case "stakeSat":
-      return provider.activeTVLSat;
-    case "commission":
-      return parseFloat(provider.commission);
-    default:
-      return 0;
-  }
 };
