dda3167c05782755861d4b6d2d282e9c8d080454
Govard Barkhatov <gbarkhatov@gmail.com>
2024-08-16 14:19:53 +0200

    responsive clean

--- src/app/components/Summary/Summary.tsx
+++ src/app/components/Summary/Summary.tsx
@@ -25,7 +25,7 @@ export const Summary: React.FC<SummaryProps> = ({
     <div className="card flex flex-col gap-2 bg-base-300 p-4 shadow-sm xl:flex-row xl:items-center xl:justify-between xl:gap-4">
       <h3 className="mb-4 font-bold xl:mb-0">Your staking summary</h3>
       <div className="flex flex-1 justify-between gap-2">
-        <div className="flex flex-col gap-1 text-sm xl:flex-1 xl:flex-row xl:items-center xl:justify-center xl:gap-2 xl:text-base">
+        <div className="flex flex-col gap-1 text-sm xl:flex-1 xl:flex-row xl:items-center xl:justify-center xl:gap-2">
           <p className="dark:text-neutral-content">Total staked</p>
           <div className="flex items-center gap-1">
             <FaBitcoin className="text-primary" size={16} />
@@ -38,8 +38,8 @@ export const Summary: React.FC<SummaryProps> = ({
           </div>
         </div>
         <div className="divider divider-horizontal xl:m-0" />
-        <div className="flex flex-col gap-1 text-sm xl:flex-1 xl:flex-row xl:items-center xl:justify-center xl:gap-2 xl:text-base">
-          <div className="flex items-center gap-1">
+        <div className="flex flex-col gap-1 text-sm xl:flex-1 xl:flex-row xl:items-center xl:justify-center xl:gap-2">
+          <div className="flex items-center justify-end gap-1">
             <p className="dark:text-neutral-content">Stakable Balance</p>
             <span
               className="cursor-pointer text-xs"
@@ -69,7 +69,7 @@ export const Summary: React.FC<SummaryProps> = ({
               href="https://discord.com/invite/babylonglobal"
               target="_blank"
               rel="noopener noreferrer"
-              className="font-light text-primary hover:underline text-right lg:text-left"
+              className="font-light text-primary hover:underline text-right"
             >
               Get Test Tokens
             </a>
