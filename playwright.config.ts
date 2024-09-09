f81447cacb2662bb9ac05d8904cac14b4cd116f7
Govard Barkhatov <gbarkhatov@gmail.com>
2024-09-09 22:09:36 +0300

    rm dotenv

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,13 +8,6 @@ const PORT = process.env.PORT || 3000;
 const baseURL = `http://localhost:${PORT}`;
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
