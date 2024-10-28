8c6baddcbc164c0ae14421f35580954d2da949a1
jeremy-babylonchain <jeremy.tsang@babylonchain.io>
2024-10-28 15:57:29 +0400

    fix params

--- src/app/api/postLogTermAcceptance.ts
+++ src/app/api/postLogTermAcceptance.ts
@@ -1,18 +1,20 @@
+import { encode } from "url-safe-base64";
+
 import { apiWrapper } from "./apiWrapper";
 
 interface TermsPayload {
   address: string;
-  publicKey: string;
+  public_key: string;
 }
 
 // postLogTermsAcceptance is used to log the terms acceptance for a given address and public key
 export const postLogTermsAcceptance = async ({
   address,
-  publicKey,
+  public_key,
 }: TermsPayload) => {
   const payload: TermsPayload = {
     address: address,
-    publicKey: publicKey,
+    public_key: encode(public_key),
   };
 
   const response = await apiWrapper(
