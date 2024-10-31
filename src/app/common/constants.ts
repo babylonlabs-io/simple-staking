c2ef3a4d33600a2fca71c8d462d60c9bceb77245
Govard Barkhatov <gbarkhatov@gmail.com>
2024-10-31 07:15:24 +0200

    filter ordinals modal show once main (#271)

--- src/app/common/constants.ts
+++ src/app/common/constants.ts
@@ -1,3 +1,4 @@
 export const OVERFLOW_TVL_WARNING_THRESHOLD = 0.2;
 export const OVERFLOW_HEIGHT_WARNING_THRESHOLD = 3;
 export const UTXO_KEY = "UTXOs";
+export const FILTER_ORDINALS_MODAL_KEY = "bbn-staking-filter-ordinals-modal";
