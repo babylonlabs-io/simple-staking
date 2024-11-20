fd5f0e5c6751d411a0ef98e83d964a1b6eda0e16
wjrjerome <wjrjerome@babylonchain.io>
2024-11-21 00:39:43 +1100

    chore: bump keplr to devnet-6

--- src/config/wallet/babylon.ts
+++ src/config/wallet/babylon.ts
@@ -1,8 +1,8 @@
 // Temporary solution until we have a stable chain registry
 // The values here shall match from https://rpc.devnet.babylonlabs.io/genesis?
 export const keplrRegistry = {
-  chainId: "devnet-4",
-  chainName: "Babylon Devnet 4",
+  chainId: "devnet-6",
+  chainName: "Babylon Devnet 6",
   chainSymbolImageUrl:
     "https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/bbn-dev/chain.png",
   rpc: "https://rpc.devnet.babylonlabs.io",
