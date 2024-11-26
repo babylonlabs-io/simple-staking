7ef28034e972494b21e4fee3d1b12bc38f3b132c
Jeremy <168515712+jeremy-babylonlabs@users.noreply.github.com>
2024-11-26 15:07:17 +1100

    integrate system stats (#382)

--- src/app/hooks/api/useSystemStats.ts
+++ src/app/hooks/api/useSystemStats.ts
@@ -1,23 +1,14 @@
+import { getSystemStats } from "@/app/api/getSystemStats";
+import { ONE_MINUTE } from "@/app/constants";
 import { useAPIQuery } from "@/app/hooks/api/useApi";
 
 export const BTC_TIP_HEIGHT_KEY = "API_STATS";
 
-const defaultValues = {
-  activeTvl: 0,
-  totalTvl: 0,
-  activeDelegations: 0,
-  totalDelegations: 0,
-  activeStakers: 0,
-  totalStakers: 0,
-  activeFinalityProviders: 0,
-  totalFinalityProviders: 0,
-};
-
-export function useSystemStats() {
+export function useSystemStats({ enabled = true }: { enabled?: boolean } = {}) {
   return useAPIQuery({
     queryKey: ["API_STATS"],
-    queryFn: () => Promise.resolve(defaultValues),
-    initialData: defaultValues,
-    placeholderData: defaultValues,
+    queryFn: () => getSystemStats(),
+    refetchInterval: ONE_MINUTE,
+    enabled,
   });
 }
