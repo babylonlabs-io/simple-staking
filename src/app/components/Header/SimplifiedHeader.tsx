54bda30e0b210257d6bc79aa7c1b5347576e12c1
Jeremy <168515712+jeremy-babylonlabs@users.noreply.github.com>
2024-11-27 01:59:30 +1100

    feat: error page (#388)
    * add ui error page
    
    * bump btc-staking-ts

--- src/app/components/Header/SimplifiedHeader.tsx
+++ src/app/components/Header/SimplifiedHeader.tsx
@@ -1,12 +1,23 @@
+import { twJoin } from "tailwind-merge";
+
 import { shouldDisplayTestingMsg } from "@/config";
 
 import { Logo } from "../Logo/Logo";
 import { TestingInfo } from "../TestingInfo/TestingInfo";
 
-export const SimplifiedHeader = () => {
+export const SimplifiedHeader = ({
+  isMinimal = false,
+}: {
+  isMinimal?: boolean;
+}) => {
   return (
-    <nav>
-      <section className="bg-primary-main h-[300px] mb-[-188px]">
+    <nav className="w-full">
+      <section
+        className={twJoin(
+          "bg-primary-main w-full",
+          isMinimal ? "h-[84px]" : "h-[300px]",
+        )}
+      >
         <div className="container h-20 py-6 px-6 mx-auto flex items-center justify-between">
           <Logo />
           <div className="flex flex-1">
