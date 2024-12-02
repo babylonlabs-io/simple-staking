a65d6afbfd56056d487a1213825300b2f2cbceca
Govard Barkhatov <gbarkhatov@gmail.com>
2024-12-02 20:39:51 +0200

    rpcRrl -> rpcUrl

--- src/app/context/tomo/TomoProvider.tsx
+++ src/app/context/tomo/TomoProvider.tsx
@@ -6,40 +6,44 @@ import { type PropsWithChildren } from "react";
 import { getNetworkConfig } from "@/config/network.config";
 import { bbnDevnet } from "@/config/wallet/babylon";
 
+type ChainType = "bitcoin" | "cosmos";
+type ThemeType = "dark" | "light";
+
 export const TomoConnectionProvider = ({ children }: PropsWithChildren) => {
   const { resolvedTheme } = useTheme();
 
   const { mempoolApiUrl, network, networkName } = getNetworkConfig();
+
   const bitcoinChain = {
     id: 1,
     name: networkName,
-    type: "bitcoin" as any,
+    type: "bitcoin" as ChainType,
     network: network,
     backendUrls: {
       mempoolUrl: mempoolApiUrl + "/api/",
     },
   };
 
+  const cosmosChain = {
+    id: 2,
+    name: bbnDevnet.chainName,
+    type: "cosmos" as ChainType,
+    network: bbnDevnet.chainId,
+    modularData: bbnDevnet,
+    backendUrls: {
+      rpcUrl: bbnDevnet.rpc,
+    },
+    logo: bbnDevnet.chainSymbolImageUrl,
+  };
+
   return (
     <TomoContextProvider
       bitcoinChains={[bitcoinChain]}
       chainTypes={["bitcoin", "cosmos"]}
-      cosmosChains={[
-        {
-          id: 2,
-          name: bbnDevnet.chainName,
-          type: "cosmos",
-          network: bbnDevnet.chainId,
-          modularData: bbnDevnet,
-          backendUrls: {
-            rpcRrl: bbnDevnet.rpc,
-          },
-          logo: bbnDevnet.chainSymbolImageUrl,
-        },
-      ]}
+      cosmosChains={[cosmosChain]}
       style={{
         rounded: "medium",
-        theme: resolvedTheme as "dark" | "light",
+        theme: resolvedTheme as ThemeType,
         primaryColor: "#FF7C2A",
       }}
     >
