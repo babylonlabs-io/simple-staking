de94875c3e0f87d21a1c4812af108437d6ecc7c2
wjrjerome <wjrjerome@babylonchain.io>
2024-12-09 18:33:57 +1100

    feat: add phase-1 unbonding method

--- src/app/components/Modals/UnbondModal.tsx
+++ src/app/components/Modals/UnbondModal.tsx
@@ -10,6 +10,7 @@ import {
 } from "@babylonlabs-io/bbn-core-ui";
 
 import { useIsMobileView } from "@/app/hooks/useBreakpoint";
+import { getNetworkConfig } from "@/config/network.config";
 
 interface UnbondModalProps {
   isOpen: boolean;
@@ -24,6 +25,7 @@ export const UnbondModal = ({
   onProceed,
   awaitingWalletResponse,
 }: UnbondModalProps) => {
+  const { networkName, coinName } = getNetworkConfig();
   const isMobileView = useIsMobileView();
 
   const DialogComponent = isMobileView ? MobileDialog : Dialog;
@@ -38,8 +40,8 @@ export const UnbondModal = ({
       <DialogBody className="pb-8 pt-4 text-primary-dark">
         <Text variant="body1">
           You are about to unbond your stake before its expiration. A
-          transaction fee of 0.00005 Signet BTC will be deduced from your stake
-          by the BTC signet network.
+          transaction fee of 0.00005 {coinName} will be deduced from your stake
+          by the {networkName} network.
           <br />
           <br />
           The expected unbonding time will be about 7 days. After unbonded, you
