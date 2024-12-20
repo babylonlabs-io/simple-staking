f0da2d0575c40dbcfe6769e558de847c9bb1a32a
wjrjerome <wjrjerome@babylonchain.io>
2024-12-20 14:39:37 +1100

    fix: use param data instead of hardcoded unbonding fee and confirmation depth

--- src/app/components/Modals/InfoModal.tsx
+++ src/app/components/Modals/InfoModal.tsx
@@ -8,16 +8,29 @@ import {
   Text,
 } from "@babylonlabs-io/bbn-core-ui";
 
+import { useNetworkInfo } from "@/app/hooks/client/api/useNetworkInfo";
 import { useIsMobileView } from "@/app/hooks/useBreakpoint";
+import { getNetworkConfig } from "@/config/network.config";
+import { blocksToDisplayTime } from "@/utils/time";
 
 interface InfoModalProps {
   open: boolean;
   onClose: () => void;
 }
 
+const { coinName } = getNetworkConfig();
+
 export function InfoModal({ open, onClose }: InfoModalProps) {
+  const { data: networkInfo } = useNetworkInfo();
   const isMobileView = useIsMobileView();
   const DialogComponent = isMobileView ? MobileDialog : Dialog;
+
+  const unbondingTime = blocksToDisplayTime(
+    networkInfo?.params.bbnStakingParams?.latestParam?.unbondingTime,
+  );
+  const maxStakingPeriod = blocksToDisplayTime(
+    networkInfo?.params.bbnStakingParams?.latestParam?.maxStakingTimeBlocks,
+  );
 
   return (
     <DialogComponent open={open} onClose={onClose}>
@@ -29,15 +42,15 @@ export function InfoModal({ open, onClose }: InfoModalProps) {
       <DialogBody className="flex flex-col pb-8 pt-4 text-primary-dark gap-4">
         <div className="py-4 flex flex-col items-start gap-4">
           <Text variant="body1">
-            Stakes made through this dashboard are locked for up to 65 weeks.
-            You can on-demand unbond at any time, with withdrawal available
-            after a 7-day unbonding period. If the maximum staking period
-            expires, your stake becomes withdrawable automatically, with no need
-            for prior unbonding.
+            Stakes made through this dashboard are locked for up to{" "}
+            {maxStakingPeriod}. You can on-demand unbond at any time, with
+            withdrawal available after a {unbondingTime} unbonding period. If
+            the maximum staking period expires, your stake becomes withdrawable
+            automatically, with no need for prior unbonding.
           </Text>
           <Text variant="body1" className="text-gray-500 italic">
-            Note: Timeframes are approximate, based on an average Bitcoin block
-            time of 10 minutes.
+            Note: Timeframes are approximate, based on an average {coinName}{" "}
+            block time of 10 minutes.
           </Text>
         </div>
       </DialogBody>
--- src/app/components/Modals/UnbondModal.tsx
+++ src/app/components/Modals/UnbondModal.tsx
@@ -1,6 +1,10 @@
 import { Text } from "@babylonlabs-io/bbn-core-ui";
 
+import { useNetworkInfo } from "@/app/hooks/client/api/useNetworkInfo";
 import { getNetworkConfig } from "@/config/network.config";
+import { satoshiToBtc } from "@/utils/btc";
+import { maxDecimals } from "@/utils/maxDecimals";
+import { blocksToDisplayTime } from "@/utils/time";
 
 import { ConfirmationModal } from "./ConfirmationModal";
 
@@ -18,6 +22,17 @@ export const UnbondModal = ({
   onSubmit,
   processing,
 }: UnbondModalProps) => {
+  const { data: networkInfo } = useNetworkInfo();
+  const unbondingTime = blocksToDisplayTime(
+    networkInfo?.params.bbnStakingParams?.latestParam?.unbondingTime,
+  );
+  const unbondingFeeBtc = maxDecimals(
+    satoshiToBtc(
+      networkInfo?.params.bbnStakingParams?.latestParam?.unbondingFeeSat ?? 0,
+    ),
+    8,
+  );
+
   return (
     <ConfirmationModal
       title="Unbonding"
@@ -28,13 +43,13 @@ export const UnbondModal = ({
     >
       <Text variant="body1" className="pb-8 pt-4">
         You are about to unbond your stake before its expiration. A transaction
-        fee of 0.00005 {coinName} will be deduced from your stake by the{" "}
-        {networkName} network.
+        fee of {unbondingFeeBtc} {coinName} will be deduced from your stake by
+        the {networkName} network.
         <br />
         <br />
-        The expected unbonding time will be about 7 days. After unbonded, you
-        will need to use this dashboard to withdraw your stake for it to appear
-        in your wallet.
+        The expected unbonding time will be about {unbondingTime}. After
+        unbonded, you will need to use this dashboard to withdraw your stake for
+        it to appear in your wallet.
       </Text>
     </ConfirmationModal>
   );
