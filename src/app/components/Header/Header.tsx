ac97fb7e5a382153f1c9272bf0203e129a517cb6
Tong Li <tongli0809@gmail.com>
2025-01-05 11:47:18 +1100

    adjust stats position

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -11,7 +11,7 @@ export const Header = () => {
 
   return (
     <nav>
-      <section className="bg-primary-main h-[300px] -mb-[250px] md:-mb-[188px]">
+      <section className="bg-primary-main h-[300px] -mb-[208px] md:-mb-[128px]">
         <div className="container h-20 py-6 px-6 mx-auto flex items-center justify-between">
           <Logo />
           <div className="flex items-center gap-4">
