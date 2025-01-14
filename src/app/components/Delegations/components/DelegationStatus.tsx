170543b3aa4dbdfe9ff9bec10efaa552b231250c
Jeremy <168515712+jeremy-babylonlabs@users.noreply.github.com>
2025-01-14 20:54:46 +0800

    update overflow tags (#603)

--- src/app/components/Delegations/components/DelegationStatus.tsx
+++ src/app/components/Delegations/components/DelegationStatus.tsx
@@ -1,21 +1,27 @@
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import { Tooltip } from "react-tooltip";
+import { twJoin } from "tailwind-merge";
 
 interface DelegationStatusProps {
   state: string;
   tooltip: string;
   stakingTxHashHex: string;
+  isOverflow?: boolean;
 }
 
 export const DelegationStatus: React.FC<DelegationStatusProps> = ({
   state,
   tooltip,
   stakingTxHashHex,
+  isOverflow,
 }) => (
   <div className="flex items-center justify-start gap-1">
-    <p>{state}</p>
+    <p className={twJoin(isOverflow ? "text-warning-main" : "")}>{state}</p>
     <span
-      className="cursor-pointer text-xs"
+      className={twJoin(
+        "cursor-pointer text-xs",
+        isOverflow ? "text-warning-main" : "",
+      )}
       data-tooltip-id={`tooltip-${stakingTxHashHex}`}
       data-tooltip-content={tooltip}
       data-tooltip-place="top"
