c11691d7545f107e38d6a5b7df250b9e4235c3a9
David Totraev <totraev@gmail.com>
2025-02-11 00:28:36 +0500

    feat: validate address & pk

--- src/utils/wallet/index.ts
+++ src/utils/wallet/index.ts
@@ -1,9 +1,11 @@
-import { networks } from "bitcoinjs-lib";
+import { networks, payments } from "bitcoinjs-lib";
+import { toXOnly } from "bitcoinjs-lib/src/psbt/bip371";
 
 import { Network } from "./wallet_provider";
 
 const nativeSegwitAddressLength = 42;
 const taprootAddressLength = 62;
+export const COMPRESSED_PUBLIC_KEY_HEX_LENGTH = 66;
 
 export const toNetwork = (network: Network): networks.Network => {
   switch (network) {
@@ -32,3 +34,56 @@ export const getPublicKeyNoCoord = (pkHex: string): Buffer => {
   const publicKey = Buffer.from(pkHex, "hex");
   return publicKey.subarray(1, 33);
 };
+
+export const getTaprootAddress = (publicKey: string, network: Network) => {
+  if (publicKey.length == COMPRESSED_PUBLIC_KEY_HEX_LENGTH) {
+    publicKey = publicKey.slice(2);
+  }
+
+  const internalPubkey = Buffer.from(publicKey, "hex");
+  const { address, output: scriptPubKey } = payments.p2tr({
+    internalPubkey: toXOnly(internalPubkey),
+    network: toNetwork(network),
+  });
+
+  if (!address || !scriptPubKey) {
+    throw new Error(
+      "Failed to generate taproot address or script from public key",
+    );
+  }
+
+  return address;
+};
+
+export const getNativeSegwitAddress = (publicKey: string, network: Network) => {
+  if (publicKey.length !== COMPRESSED_PUBLIC_KEY_HEX_LENGTH) {
+    throw new Error(
+      "Invalid public key length for generating native segwit address",
+    );
+  }
+
+  const internalPubkey = Buffer.from(publicKey, "hex");
+  const { address, output: scriptPubKey } = payments.p2wpkh({
+    pubkey: internalPubkey,
+    network: toNetwork(network),
+  });
+
+  if (!address || !scriptPubKey) {
+    throw new Error(
+      "Failed to generate native segwit address or script from public key",
+    );
+  }
+
+  return address;
+};
+
+export function validateAddressWithPK(
+  address: string,
+  publicKey: string,
+  network: Network,
+) {
+  return (
+    address === getTaprootAddress(publicKey, network) ||
+    address === getNativeSegwitAddress(publicKey, network)
+  );
+}
