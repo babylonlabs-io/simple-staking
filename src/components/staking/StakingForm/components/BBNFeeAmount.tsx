f14c46eadedc4979464b102aed15c48451c537da
jeremy-babylonchain <jeremy.tsang@babylonchain.io>
2025-02-14 03:49:16 +0700

    resolve comments

--- src/components/staking/StakingForm/components/BBNFeeAmount.tsx
+++ src/components/staking/StakingForm/components/BBNFeeAmount.tsx
@@ -1,3 +1,4 @@
+import { usePrices } from "@/app/hooks/client/api/usePrices";
 import { getNetworkConfigBBN } from "@/config/network/bbn";
 
 import { FeeItem } from "./FeeItem";
@@ -9,8 +10,14 @@ interface FeeStatsProps {
 const { coinSymbol } = getNetworkConfigBBN();
 
 export function BBNFeeAmount({ amount = "0" }: FeeStatsProps) {
+  const { data: prices } = usePrices();
+
+  const bbnInUsd = prices?.[coinSymbol] ?? 0;
+  const feeInUsd = (parseFloat(amount) * bbnInUsd).toFixed(2);
+  const feeInUsdDisplay = feeInUsd === "0.00" ? "-" : `$${feeInUsd}`;
+
   return (
-    <FeeItem title={`${coinSymbol} Network Fee`}>
+    <FeeItem title={`${coinSymbol} Network Fee`} hint={feeInUsdDisplay}>
       {amount} {coinSymbol}
     </FeeItem>
   );
