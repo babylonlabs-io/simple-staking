ec1c576e0a470e7a447b12c9df710c7e19c03a42
Tong Li <supertong@users.noreply.github.com>
2025-03-11 16:04:58 +1100

    Tong/change to static export (#347)
    * change to static export
    
    * add s3 publish step
    
    * update dockerfile for backward compatible and remove upload artifacts on github
    
    * update AWS_S3_REGION
    
    ---------
    
    Co-authored-by: liam.lai <liam.lai@us>

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@
 import { withSentryConfig } from "@sentry/nextjs";
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone",
+  swcMinify: false,
+  output: "export",
+  images: { unoptimized: true },
   experimental: {
     forceSwcTransforms: true,
   },
