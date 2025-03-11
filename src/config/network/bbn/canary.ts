9d63ec4fd50b2430c642390820f740d3a7031cf4
David Totraev <totraev@gmail.com>
2025-03-11 23:26:45 +0500

    feat: add mock network

--- src/config/network/bbn/canary.ts
+++ src/config/network/bbn/canary.ts
@@ -7,8 +7,8 @@ export const BBN_CANARY_LCD_URL = process.env.NEXT_PUBLIC_BABY_LCD_URL
   : "https://lcd.btc-mainnet.babylonlabs.io/";
 
 export const bbnCanary = {
-  chainId: "bbn-priv-main-1",
-  chainName: "Babylon Private Mainnet",
+  chainId: "bbn-mock-1",
+  chainName: "Babylon Mock Mainnet",
   chainSymbolImageUrl:
     "https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/bbn-test/chain.png",
   rpc: BBN_CANARY_RPC_URL,
