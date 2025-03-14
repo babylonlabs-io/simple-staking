6b897c842c03392f49e9ad43cc6cce579bedb5e2
jeremy-babylonchain <jeremy.tsang@babylonchain.io>
2025-03-14 17:43:39 +0700

    adjust sample rate

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -34,16 +34,16 @@ Sentry.init({
   // Reference: https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables
   release: process.env.GITHUB_SHA ?? "NA",
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: 0,
+
+  enableTracing: false,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+
   replaysOnErrorSampleRate: 1.0,
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 0,
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
