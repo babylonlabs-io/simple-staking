f3ebf2b8a328928881d2189088f361b4e26b9959
David Totrashvili <8580261+0xDazzer@users.noreply.github.com>
2025-03-27 18:17:00 +0500

    fix: address screening error (#810)

--- src/app/api/logTermAcceptance.ts
+++ src/app/api/logTermAcceptance.ts
@@ -14,15 +14,17 @@ export const logTermsAcceptance = async ({
   address,
   public_key,
 }: TermsPayload) => {
-  const payload: TermsPayload = {
-    address: address,
-    public_key: encode(getPublicKeyNoCoord(public_key).toString("hex")),
-  };
+  try {
+    const payload: TermsPayload = {
+      address: address,
+      public_key: encode(getPublicKeyNoCoord(public_key).toString("hex")),
+    };
 
-  const response = await apiWrapper<unknown>(
-    "POST",
-    "/log-terms-acceptance",
-    "Error submitting terms acceptance request",
-    { body: payload },
-  );
+    await apiWrapper<unknown>(
+      "POST",
+      "/log-terms-acceptance",
+      "Error submitting terms acceptance request",
+      { body: payload },
+    );
+  } catch {}
 };
