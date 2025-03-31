96cf076aad54ac0203b66a444f9a5776d12623d6
David Totraev <totraev@gmail.com>
2025-03-31 19:37:52 +0500

    feat: update staking stats

--- src/utils/formatBTCTvl.ts
+++ src/utils/formatBTCTvl.ts
@@ -3,13 +3,13 @@ import { maxDecimals } from "./maxDecimals";
 export function formatBTCTvl(
   tvlInBtc: number,
   coinSymbol: string,
-  usdValue?: number,
+  rate?: number,
 ): string {
   const btcFormatted =
     tvlInBtc >= 1 ? maxDecimals(tvlInBtc, 2) : maxDecimals(tvlInBtc, 8);
 
-  const usdPart = usdValue
-    ? `($${usdValue.toLocaleString("en", {
+  const usdPart = rate
+    ? `($${(tvlInBtc * rate).toLocaleString("en", {
         notation: "compact",
         maximumFractionDigits: 2,
       })})`
