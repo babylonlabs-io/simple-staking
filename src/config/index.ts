482def9d9079ce42038763a439798fdb763e2dc4
Jony Bursztyn <jonathan@bursztyn.io>
2025-04-11 10:40:41 +0200

    feat: defaults FP filter to inactive (#850)
    * Switches FP Filter to Inactive if there are no Active FPs
    
    * Switch default Select to inactive
    
    * Make FP filter state be handled through env variable
    
    * Move function to config file
    
    * Change function for a const
    
    * Change type

--- src/config/index.ts
+++ src/config/index.ts
@@ -16,6 +16,11 @@ export const getNetworkAppUrl = (): string => {
     : "https://btcstaking.babylonlabs.io";
 };
 
+export const DEFAULT_FILTER_VALUE: "active" | "inactive" =
+  process.env.NEXT_PUBLIC_DEFAULT_FP_FILTER === "inactive"
+    ? "inactive"
+    : "active";
+
 export const IS_FIXED_TERM_FIELD =
   process.env.NEXT_PUBLIC_FIXED_STAKING_TERM === "true";
 
