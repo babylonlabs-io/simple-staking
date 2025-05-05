5734c01ab69fba2ed5d8b9f5adddcbfec87792c9
jeremy-babylonchain <jeremy.tsang@babylonchain.io>
2025-05-05 19:07:28 +0700

    resolve comments

--- src/errors/clientError.ts
+++ src/errors/clientError.ts
@@ -1,16 +1,13 @@
-import { ErrorCode } from "./codes";
-
 interface ClientErrorOptions extends ErrorOptions {
   metadata?: Record<string, any>;
 }
 
 export class ClientError extends Error {
   constructor(
-    public errorCode: ErrorCode,
+    public readonly errorCode: string,
     message: string,
     options?: ClientErrorOptions,
   ) {
     super(message, options);
-    this.name = this.constructor.name;
   }
 }
