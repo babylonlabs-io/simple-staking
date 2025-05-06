bdb91cc48f3f6d0aeed296a02f1c7f52eb4632a5
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-05-06 10:25:17 +0100

    Add another mock

--- e2e/mocks/blockchain.ts
+++ e2e/mocks/blockchain.ts
@@ -764,4 +764,52 @@ export const injectBBNQueries = async (
       }),
     });
   });
+
+  await page.route("**/v2/network-info*", async (route) => {
+    await route.fulfill({
+      status: 200,
+      contentType: "application/json",
+      body: JSON.stringify({
+        data: {
+          staking_status: {
+            is_staking_open: true,
+          },
+          params: {
+            bbn: [
+              {
+                version: 0,
+                covenant_pks: [
+                  "0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798",
+                  "02c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5",
+                  "02f9308a019258c31049344f85f89d5229b531c845836f99b08601f113bce036f9",
+                ],
+                covenant_quorum: 2,
+                min_staking_value_sat: 100000,
+                max_staking_value_sat: 1000000,
+                min_staking_time_blocks: 144,
+                max_staking_time_blocks: 1440,
+                slashing_pk_script:
+                  "76a914c96e00cdddfe208629638e394358b2770e636b2388ac",
+                min_slashing_tx_fee_sat: 1000,
+                slashing_rate: "0.1",
+                unbonding_time_blocks: 144,
+                unbonding_fee_sat: 2000,
+                min_commission_rate: "0.05",
+                max_active_finality_providers: 10,
+                delegation_creation_base_gas_fee: 50000,
+                btc_activation_height: 100,
+                allow_list_expiration_height: 200,
+              },
+            ],
+            btc: [
+              {
+                version: 0,
+                btc_confirmation_depth: 6,
+              },
+            ],
+          },
+        },
+      }),
+    });
+  });
 };
