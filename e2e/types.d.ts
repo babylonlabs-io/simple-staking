088c6fadba1355bacdc9bd3e470323366a93febe
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-05-09 00:00:48 +0100

    Revert bbnQueries.ts

--- e2e/types.d.ts
+++ e2e/types.d.ts
@@ -1,5 +1,19 @@
 declare global {
   interface Window {
+    // From balanceAddress.spec.ts
+    mockCosmJSBankBalance: (
+      address: string,
+    ) => Promise<{ amount: string; denom: string }>;
+    mockCosmJSRewardsQuery: (address: string) => Promise<{
+      rewardGauges: {
+        [key: string]: {
+          coins: Array<{ amount: string; denom: string }>;
+          withdrawnCoins: Array<{ amount: string; denom: string }>;
+        };
+      };
+    }>;
+
+    // From handlers.ts
     require: any;
     __e2eTestMode: boolean;
     __mockVerifyBTCAddress: () => Promise<boolean>;
