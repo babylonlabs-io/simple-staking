7f5a326db9616035293c65c16a114c196840c0c7
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-05-11 23:11:36 +0100

    Add firefox and webkit

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,11 @@ export default defineConfig({
     trace: "on-first-retry",
   },
 
-  projects: [{ name: "chromium", use: devices["Desktop Chrome"] }],
+  projects: [
+    { name: "chromium", use: devices["Desktop Chrome"] },
+    { name: "firefox", use: devices["Desktop Firefox"] },
+    { name: "webkit", use: devices["Desktop Safari"] },
+  ],
 
   webServer: {
     command: `sh -c 'NODE_OPTIONS="--max-http-header-size=65536" PORT=${PORT} npx next start -H 0.0.0.0 -p ${PORT}'`,
