b0e32dfdc618ce619c87ef97a44546bd840c1e7b
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-05-11 23:08:21 +0100

    Set up e2e types

--- e2e/types.d.ts
+++ e2e/types.d.ts
@@ -13,10 +13,40 @@ declare global {
       };
     }>;
 
-    // From handlers.ts
     require: any;
     __e2eTestMode: boolean;
     __mockVerifyBTCAddress: () => Promise<boolean>;
+
+    btcwallet: {
+      connectWallet: () => any;
+      getWalletProviderName: () => string;
+      getAddress: () => string;
+      getPublicKeyHex: () => string;
+      on: (...args: any[]) => any;
+      off?: (event: string, callback: Function) => void;
+      getNetwork: () => string;
+      getBTCTipHeight: () => number;
+      getNetworkFees: () => {
+        fastestFee: number;
+        halfHourFee: number;
+        hourFee: number;
+        economyFee: number;
+        minimumFee: number;
+      };
+      getInscriptions: () => any[];
+      signPsbt: (psbtHex: string) => string;
+      pushTx: (txHex: string) => string;
+      isConnected?: boolean;
+    };
+
+    bbnwallet: {
+      connectWallet: () => any;
+      getWalletProviderName: () => string;
+      getOfflineSigner: () => any;
+    };
+
+    unisat?: any;
+    $onekey?: any;
   }
 }
 
