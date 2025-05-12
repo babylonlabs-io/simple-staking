e69431ceac8199d86e9ca43514bbb3afbf27306e
jeremy-babylonchain <jeremy.tsang@babylonchain.io>
2025-05-12 13:53:10 +0700

    refactor: add missing metadata to clientClass body

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,15 +1,17 @@
 interface ClientErrorOptions extends ErrorOptions {
-  cause?: Error;
   metadata?: Record<string, any>;
 }
 
 export class ClientError extends Error {
+  public readonly metadata?: Record<string, any>;
+
   constructor(
     public readonly errorCode: string,
     message: string,
     options?: ClientErrorOptions,
   ) {
     super(message, options);
+    if (options?.metadata) this.metadata = options.metadata;
   }
 }
 
