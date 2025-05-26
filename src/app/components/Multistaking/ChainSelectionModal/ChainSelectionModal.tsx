4b8f44fbf1c57a520b3f044ceda7b590cf483fe0
Jony Bursztyn <jonathan@bursztyn.io>
2025-05-26 13:40:53 +0200

    feat: adds ChainSelectionModal (#1026)
    * Add ChainSelectionModal
    
    * Refactor to use tailwind

---
+++ src/app/components/Multistaking/ChainSelectionModal/ChainSelectionModal.tsx
@@ -0,0 +1,156 @@
+import {
+  Button,
+  DialogBody,
+  DialogFooter,
+  DialogHeader,
+  Text,
+} from "@babylonlabs-io/core-ui";
+import Image, { StaticImageData } from "next/image";
+import { PropsWithChildren, useState } from "react";
+import { MdOutlineInfo } from "react-icons/md";
+import { twMerge } from "tailwind-merge";
+
+import babylon from "@/app/assets/babylon-genesis.png";
+import cosmos from "@/app/assets/cosmos.png";
+import ethereum from "@/app/assets/ethereum.png";
+import sui from "@/app/assets/sui.png";
+
+const SubSection = ({
+  children,
+  style,
+  className,
+}: {
+  children: React.ReactNode;
+  style?: React.CSSProperties;
+  className?: string;
+}) => (
+  <div
+    className={twMerge(
+      "flex bg-secondary-highlight text-[#12495E] p-4",
+      className,
+    )}
+    style={style}
+  >
+    {children}
+  </div>
+);
+
+interface ChainButtonProps extends PropsWithChildren {
+  className?: string;
+  disabled?: boolean;
+  logo?: string | StaticImageData;
+  title?: string | JSX.Element;
+  alt?: string;
+  selected?: boolean;
+  onClick?: () => void;
+}
+
+const ChainButton = ({
+  className,
+  disabled,
+  title,
+  logo,
+  selected,
+  onClick,
+}: ChainButtonProps) => (
+  <Text
+    disabled={disabled}
+    as={disabled ? "div" : "button"}
+    className={twMerge(
+      "bg-[#F9F9F9] w-full py-[14px] px-6 pl-[14px] rounded border",
+      selected ? "border-[#CE6533]" : "border-transparent",
+      disabled
+        ? "opacity-50 pointer-events-none cursor-default"
+        : "cursor-pointer",
+      className,
+    )}
+    onClick={onClick}
+  >
+    <div className="flex w-full items-center justify-between">
+      <div className="flex items-center text-base">
+        {logo && (
+          <Image
+            src={logo}
+            alt="bitcoin"
+            className="max-w-[40px] max-h-[40px] mr-2 rounded-full"
+          />
+        )}
+        {title}
+      </div>
+      <div className="text-xs">TVL: X BTC ($Y)</div>
+    </div>
+  </Text>
+);
+
+export const ChainSelectionModal = ({
+  onNext,
+  onClose,
+}: {
+  onNext: (selectedChain: string) => void;
+  onClose: () => void;
+}) => {
+  const [selected, setSelected] = useState<string | null>(null);
+
+  return (
+    <>
+      <DialogHeader
+        title="Select Available BSN"
+        onClose={onClose}
+        className="text-accent-primary"
+      />
+
+      <DialogBody className="flex flex-col mb-4 mt-4 text-accent-primary">
+        <div>
+          Bitcoin Supercharged Networks (BSNs) are Proof-of-Stake chains secured
+          by Bitcoin staking. Select a network to delegate your stake and earn
+          rewards.
+        </div>
+        <div className="overflow-x-auto flex flex-col gap-2 mt-10">
+          <ChainButton
+            logo={babylon}
+            title="Babylon Genesis"
+            selected={selected === "babylon"}
+            onClick={() => setSelected("babylon")}
+          />
+          <ChainButton
+            logo={cosmos}
+            title="Cosmos"
+            disabled
+            onClick={() => setSelected("cosmos")}
+          />
+          <ChainButton
+            logo={ethereum}
+            title="Ethereum"
+            disabled
+            onClick={() => setSelected("ethereum")}
+          />
+          <ChainButton
+            logo={sui}
+            title="Sui"
+            disabled
+            onClick={() => setSelected("sui")}
+          />
+        </div>
+        <SubSection className="text-base text-[#387085] gap-3 flex-row mt-4">
+          <div>
+            <MdOutlineInfo size={22} />
+          </div>
+          <div>
+            Babylon must be the first BSN you add before selecting others. Once
+            added, you can choose additional BSNs to multistake.
+          </div>
+        </SubSection>
+      </DialogBody>
+
+      <DialogFooter className="flex justify-end">
+        <Button
+          variant="contained"
+          onClick={() => selected && onNext(selected)}
+          disabled={!selected}
+        >
+          Next
+        </Button>
+      </DialogFooter>
+    </>
+  );
+};
