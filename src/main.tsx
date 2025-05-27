7d92cc90d40f517b98670f59a17309955e7d39f5
David Totrashvili <8580261+0xDazzer@users.noreply.github.com>
2025-05-27 20:42:28 +0500

    fix: error boundaries (#1039)

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ErrorBoundary } from "react-error-boundary";
 import "./app/globals.css";
 
+import GlobalError from "@/app/global-error";
 import Layout from "@/app/layout";
 import HomePage from "@/app/page";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Layout>
-      <HomePage />
+      <ErrorBoundary FallbackComponent={GlobalError}>
+        <HomePage />
+      </ErrorBoundary>
     </Layout>
   </StrictMode>,
 );
