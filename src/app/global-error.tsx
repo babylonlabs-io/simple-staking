a4b812423bda5128534ff92469d76d083d5df81a
David Totraev <totraev@gmail.com>
2025-05-27 20:39:57 +0500

    fix: error boundaries

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -4,7 +4,6 @@ import { useEffect } from "react";
 
 import GenericError from "@/app/components/Error/GenericError";
 import { useError } from "@/app/context/Error/ErrorProvider";
-import { Error } from "@/app/types/errors";
 
 export default function GlobalError({ error }: { error: Error }) {
   const { handleError } = useError();
