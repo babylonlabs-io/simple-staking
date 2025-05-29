486a8f23501585ae69d37da03cba94cfa09c4d9b
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-05-29 15:11:51 +0200

    Revert changes

--- src/app/components/Staking/FinalityProviders/FinalityProviders.tsx
+++ src/app/components/Staking/FinalityProviders/FinalityProviders.tsx
@@ -9,37 +9,25 @@ import { FinalityProviderFilter } from "./FinalityProviderFilter";
 import { FinalityProviderSearch } from "./FinalityProviderSearch";
 import { FinalityProviderTable } from "./FinalityProviderTable";
 
-export const FinalityProviders = ({
-  showHeader = true,
-  showFilter = true,
-}: {
-  showHeader?: boolean;
-  showFilter?: boolean;
-}) => {
+export const FinalityProviders = () => {
   const { setValue } = useFormContext();
 
   return (
     <div className="flex flex-col gap-4">
-      {showHeader && (
-        <>
-          <Heading variant="h5" className="text-accent-primary">
-            Step 1
-          </Heading>
-          <Text variant="body1" className="text-accent-secondary">
-            Select a Finality Provider
-          </Text>
-        </>
-      )}
+      <Heading variant="h5" className="text-accent-primary">
+        Step 1
+      </Heading>
+      <Text variant="body1" className="text-accent-secondary">
+        Select a Finality Provider
+      </Text>
 
       <div className="flex flex-col md:flex-row gap-4">
         <div className="flex-1">
           <FinalityProviderSearch />
         </div>
-        {showFilter && (
-          <div className="w-full md:w-[200px]">
-            <FinalityProviderFilter />
-          </div>
-        )}
+        <div className="w-full md:w-[200px]">
+          <FinalityProviderFilter />
+        </div>
       </div>
 
       <FinalityProviderTable
