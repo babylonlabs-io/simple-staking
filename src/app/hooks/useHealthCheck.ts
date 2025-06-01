28572a6ccce0b842ca245693706a1ca1e834dbdc
jeremy-babylonchain <jeremy.tsang@babylonchain.io>
2025-06-01 16:45:45 +0800

    fix: healthcheck throw client error

--- src/app/hooks/useHealthCheck.ts
+++ src/app/hooks/useHealthCheck.ts
@@ -1,28 +1,30 @@
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 
-import {
-  getHealthCheck,
-  isGeoBlockedResult,
-} from "@/app/services/healthCheckService";
+import { getHealthCheck } from "@/app/services/healthCheckService";
 import { HealthCheckStatus } from "@/app/types/services/healthCheck";
+import { ClientError, ERROR_CODES } from "@/errors";
 import { useLogger } from "@/hooks/useLogger";
 
 import { useError } from "../context/Error/ErrorProvider";
+
+export const HEALTH_CHECK_KEY = "HEALTH_CHECK";
 
 export const useHealthCheck = () => {
   const { handleError } = useError();
   const logger = useLogger();
 
   const { data, error, isError, isLoading, refetch } = useQuery({
-    queryKey: ["api available"],
+    queryKey: [HEALTH_CHECK_KEY],
     queryFn: getHealthCheck,
     refetchOnMount: false,
     refetchOnWindowFocus: false,
   });
 
   const isApiNormal = data?.status === HealthCheckStatus.Normal;
-  const isGeoBlocked = data ? isGeoBlockedResult(data) : false;
+  const isGeoBlocked = error
+    ? (error as ClientError).errorCode === ERROR_CODES.GEO_BLOCK
+    : false;
   const apiMessage = data?.message;
 
   useEffect(() => {
