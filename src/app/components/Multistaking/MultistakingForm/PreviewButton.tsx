c0d2de399a43380d844df8ad4ab1521816c747fe
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-06-03 17:29:43 +0200

    Implement connect button

--- src/app/components/Multistaking/MultistakingForm/PreviewButton.tsx
+++ src/app/components/Multistaking/MultistakingForm/PreviewButton.tsx
@@ -1,7 +1,11 @@
 import { Button, useFormState } from "@babylonlabs-io/core-ui";
 import { twJoin } from "tailwind-merge";
 
+import { useBTCWallet } from "@/app/context/wallet/BTCWalletProvider";
+import { AuthGuard } from "@/components/common/AuthGuard";
+
 export function PreviewButton() {
+  const { open } = useBTCWallet();
   const { isValid, errors } = useFormState();
   const errorKeys = Object.keys(errors);
   const errorMessages = errorKeys.map((key) => errors[key]?.message);
@@ -16,17 +20,30 @@ export function PreviewButton() {
   };
 
   return (
-    <Button
-      //@ts-ignore - fix type issue in core-ui
-      type="submit"
-      className={twJoin(
-        "w-full mt-2",
-        hasError &&
-          "!text-[#12495E] !bg-[#EBF1F3] hover:!bg-[#EBF1F3] !brightness-100",
-      )}
-      disabled={!isValid}
+    <AuthGuard
+      fallback={
+        <Button
+          //@ts-ignore - fix type issue in core-ui
+          type="submit"
+          onClick={open}
+          className={"w-full mt-2"}
+        >
+          Connect Wallet
+        </Button>
+      }
     >
-      {renderButtonContent()}
-    </Button>
+      <Button
+        //@ts-ignore - fix type issue in core-ui
+        type="submit"
+        className={twJoin(
+          "w-full mt-2 capitalize",
+          hasError &&
+            "!text-[#12495E] !bg-[#EBF1F3] hover:!bg-[#EBF1F3] !brightness-100",
+        )}
+        disabled={!isValid}
+      >
+        {renderButtonContent()}
+      </Button>
+    </AuthGuard>
   );
 }
