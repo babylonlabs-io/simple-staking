1d7b45d41792d7e77eb0afcbb127bd4759a6e6e4
liam.lai <liam.lai@us>
2025-06-03 23:30:56 -0700

    add v4 devnet staking-expiry-checker

--- src/config/network/bbn/v4-devnet.ts
+++ src/config/network/bbn/v4-devnet.ts
@@ -1,24 +1,24 @@
 import { getUrlFromEnv } from "./urlUtils";
 
-export const BBN_DEVNET_RPC_URL = getUrlFromEnv(
+export const BBN_V4_DEVNET_RPC_URL = getUrlFromEnv(
   process.env.NEXT_PUBLIC_BABY_RPC_URL,
   "http://localhost:3000",
   "https://rpc.v4-devnet.babylonlabs.io/",
 );
 
-export const BBN_DEVNET_LCD_URL = getUrlFromEnv(
+export const BBN_V4_DEVNET_LCD_URL = getUrlFromEnv(
   process.env.NEXT_PUBLIC_BABY_LCD_URL,
   "http://localhost:1317",
   "https://lcd.v4-devnet.babylonlabs.io/",
 );
 
-export const bbnDevnet = {
+export const bbnV4Devnet = {
   chainId: "v4-devnet-1",
   chainName: "Babylon V4 Devnet 1",
   chainSymbolImageUrl:
     "https://raw.githubusercontent.com/babylonlabs-io/simple-staking/main/public/chain.png",
-  rpc: BBN_DEVNET_RPC_URL,
-  rest: BBN_DEVNET_LCD_URL,
+  rpc: BBN_V4_DEVNET_RPC_URL,
+  rest: BBN_V4_DEVNET_LCD_URL,
   nodeProvider: {
     name: "Babylonlabs",
     email: "contact@babylonlabs.io",
