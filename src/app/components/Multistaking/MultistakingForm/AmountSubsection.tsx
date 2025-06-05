dc3fd27c63aca47425bc8e7f7c3c18c70bd861d0
Jony Bursztyn <jonathan@bursztyn.io>
2025-06-05 13:40:22 +0200

    Fixes error message when typing amount (#1068)

--- src/app/components/Multistaking/MultistakingForm/AmountSubsection.tsx
+++ src/app/components/Multistaking/MultistakingForm/AmountSubsection.tsx
@@ -31,16 +31,16 @@ export const AmountSubsection = () => {
         </div>
         <input
           type="number"
+          value={btcAmount ?? ""}
           min="0"
           step="any"
-          value={btcAmount}
           onChange={handleInputChange}
           placeholder="0"
           autoFocus
           className="text-lg bg-transparent text-right w-2/3 outline-none"
         />
       </div>
-      <HiddenField name="amount" />
+      <HiddenField name="amount" defaultValue="" />
 
       <AuthGuard>
         <AmountBalanceInfo />
