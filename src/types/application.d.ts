5b8339750f4b8e53e49f3f218110b22eabe61238
David Totraev <totraev@gmail.com>
2025-06-06 17:52:56 +0500

    feat: add DI container

---
+++ src/types/application.d.ts
@@ -0,0 +1,8 @@
+namespace Application {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface DelegationUseCases {}
+}
+
+interface Application {
+  delegations: Application.DelegationUseCases;
+}
---
+++ src/types/view-models.d.ts
@@ -0,0 +1,8 @@
+namespace ViewModel {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface DelegationViewModel {}
+}
+
+interface ViewModel {
+  delegationViewModel: ViewModel.DelegationViewModel;
+}
