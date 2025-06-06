6f36dc39e1b3a75e4f214a1c09e6068e1eda1c61
David Totraev <totraev@gmail.com>
2025-06-06 17:52:56 +0500

    feat: add DI container

---
+++ src/types/domain.d.ts
@@ -0,0 +1,8 @@
+namespace Domain {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface DelegationServices {}
+}
+
+interface Domain {
+  delegationServices: Domain.DelegationServices;
+}
