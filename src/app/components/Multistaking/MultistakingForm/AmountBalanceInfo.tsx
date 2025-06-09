2cbed0dfe0aa2ab962697db3c33321b7908e5e86
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-06-09 15:23:46 +0100

    Add max balance mode

--- src/app/components/Multistaking/MultistakingForm/AmountBalanceInfo.tsx
+++ src/app/components/Multistaking/MultistakingForm/AmountBalanceInfo.tsx
@@ -1,14 +1,17 @@
 import { useFormContext, useWatch } from "@babylonlabs-io/core-ui";
+import { useEffect } from "react";
 
 import { getNetworkConfigBTC } from "@/app/config/network/btc";
 import { usePrice } from "@/app/hooks/client/api/usePrices";
 import { useBalanceState } from "@/app/state/BalanceState";
+import { useMultistakingState } from "@/app/state/MultistakingState";
 import { satoshiToBtc } from "@/app/utils/btc";
 import { calculateTokenValueInCurrency } from "@/app/utils/formatCurrency";
 import { maxDecimals } from "@/app/utils/maxDecimals";
 
 export const AmountBalanceInfo = () => {
   const { totalBtcBalance } = useBalanceState();
+  const { isMaxBalanceMode, setIsMaxBalanceMode } = useMultistakingState();
 
   const btcAmount = useWatch({ name: "amount", defaultValue: "" });
   const { setValue } = useFormContext();
@@ -30,7 +33,18 @@ export const AmountBalanceInfo = () => {
 
   const availableBalanceBtc = Math.max(balanceBtc - feeAmountBtc, 0);
 
+  useEffect(() => {
+    if (isMaxBalanceMode) {
+      setValue("amount", availableBalanceBtc.toString(), {
+        shouldValidate: true,
+        shouldDirty: true,
+        shouldTouch: true,
+      });
+    }
+  }, [isMaxBalanceMode, availableBalanceBtc, setValue]);
+
   const handleSetMaxBalance = () => {
+    setIsMaxBalanceMode(true);
     setValue("amount", availableBalanceBtc.toString(), {
       shouldValidate: true,
       shouldDirty: true,
