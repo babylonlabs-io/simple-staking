c337fefd2bf2f41ca9fd84dc16f7f01cde4d51c2
David Totrashvili <8580261+0xDazzer@users.noreply.github.com>
2025-06-11 00:14:46 +0500

    feat: configure Logger (#1097)

--- src/types/infrastructure.d.ts
+++ src/types/infrastructure.d.ts
@@ -1,11 +1,24 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 import {
   SigningStep,
   type SignPsbtOptions,
 } from "@babylonlabs-io/btc-staking-ts";
 import type { Emitter } from "nanoevents";
 
+type Context = Record<string, number | string | boolean> & {
+  category?: string;
+};
+
+type ErrorContext = {
+  level?: SeverityLevel;
+  tags?: Record<string, string>;
+  data?: Record<string, string | number | boolean>;
+};
+
 declare global {
   namespace Infra {
+    type DB = {};
+
     interface EventBusEvents {
       // Create new Delegation V2
       "delegation:create": (
@@ -33,5 +46,17 @@ declare global {
     }
 
     type EventBus = Emitter<EventBusEvents>;
+
+    interface Logger {
+      info(message: string, context?: Context): void;
+      warn(message: string, context?: Context): void;
+      error(error: Error, context?: ErrorContext): string;
+    }
+  }
+
+  interface Infra {
+    db: Infra.DB;
+    eventBus: Infra.EventBus;
+    logger: Infra.Logger;
   }
 }
