d948d29b9f7fb1eea72341461c8629919a9a2c2c
David Totraev <totraev@gmail.com>
2025-06-12 21:32:57 +0500

    feat: add config

--- src/infrastructure/bitcoin.ts
+++ src/infrastructure/bitcoin.ts
@@ -1,12 +1,12 @@
 import mempoolJS from "@mempool/mempool.js";
 
 const HTTP_PROTOCOL_REGEX = /^https?:\/\//;
-const url = process.env.NEXT_PUBLIC_MEMPOOL_API ?? "https://mempool.space";
-const hostname = url.replace(HTTP_PROTOCOL_REGEX, "");
 
-const { bitcoin } = mempoolJS({
-  hostname,
-  network: process.env.NEXT_PUBLIC_NETWORK ?? "signet",
-});
+export default ({ config }: DI.Container) => {
+  const { bitcoin } = mempoolJS({
+    hostname: config.bitcoin.url.replace(HTTP_PROTOCOL_REGEX, ""),
+    network: config.bitcoin.network,
+  });
 
-export default bitcoin;
+  return bitcoin;
+};
