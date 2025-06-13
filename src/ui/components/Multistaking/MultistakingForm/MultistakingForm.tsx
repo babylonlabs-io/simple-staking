44fe3156b55150cddaa93af919bbe06864fce682
David Totraev <totraev@gmail.com>
2025-06-13 19:11:20 +0500

    fix: remove consolo.log

--- src/ui/components/Multistaking/MultistakingForm/MultistakingForm.tsx
+++ src/ui/components/Multistaking/MultistakingForm/MultistakingForm.tsx
@@ -69,7 +69,6 @@ export function MultistakingForm() {
         mode="onChange"
         reValidateMode="onChange"
         onSubmit={handlePreview}
-        onChange={(values) => console.log(values)}
       >
         {stakingInfo && (
           <HiddenField
