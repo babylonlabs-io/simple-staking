4033603f0c11e554cba3da472b0923d3ff6ba086
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-06-17 23:20:35 +0100

    Readd chain selection modal

--- src/ui/components/Multistaking/MultistakingForm/MultistakingForm.tsx
+++ src/ui/components/Multistaking/MultistakingForm/MultistakingForm.tsx
@@ -85,7 +85,7 @@ export function MultistakingForm() {
               open={stakingModalPage === StakingModalPage.FINALITY_PROVIDER}
               max={MAX_FINALITY_PROVIDERS}
               onOpen={() =>
-                void setStakingModalPage(StakingModalPage.FINALITY_PROVIDER)
+                void setStakingModalPage(StakingModalPage.CHAIN_SELECTION)
               }
               onClose={() => void setStakingModalPage(StakingModalPage.DEFAULT)}
             />
