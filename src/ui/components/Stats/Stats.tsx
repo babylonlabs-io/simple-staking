e321910b230f6cd94a7c9b67322b3db55495b159
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-06-25 11:38:52 +0800

    wip

--- src/ui/components/Stats/Stats.tsx
+++ src/ui/components/Stats/Stats.tsx
@@ -22,8 +22,6 @@ export const Stats = memo(() => {
     data: {
       total_active_tvl: totalActiveTVL = 0,
       active_tvl: activeTVL = 0,
-      total_finality_providers: totalFPs = 0,
-      active_finality_providers: activeFPs = 0,
       btc_staking_apr: stakingAPR,
     } = {},
     isLoading,
@@ -71,13 +69,6 @@ export const Stats = memo(() => {
           title={`${coinSymbol} Staking APR`}
           value={`${formatter.format(stakingAPR ? stakingAPR * 100 : 0)}%`}
           tooltip="Annual Percentage Reward (APR) is a dynamic estimate of the annualized staking reward rate based on current network conditions, and it refers to staking rewards rather than traditional lending interest. Rewards are distributed in BABY tokens but shown as a Bitcoin-equivalent rate relative to the Bitcoin initially staked. APR is calculated using U.S. dollar values for Bitcoin and BABY from independent, reputable sources. The APR shown is an approximate figure that can fluctuate, and the displayed value may not always be completely accurate. Actual rewards are not guaranteed and may vary over time. Staking carries exposure to slashing and other risks"
-        />
-
-        <StatItem
-          loading={isLoading}
-          title="Finality Providers"
-          value={`${formatter.format(activeFPs)} Active (${formatter.format(totalFPs)} Total)`}
-          tooltip="Active and total number of Finality Providers"
         />
       </List>
     </Section>
