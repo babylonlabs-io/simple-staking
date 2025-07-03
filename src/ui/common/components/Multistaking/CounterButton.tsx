72c1656f82d5988bd08cf3ef0c92120847ed306f
jeremy-babylonchain <jeremy.tsang@babylonchain.io>
2025-07-03 19:52:36 +0800

    fix: update clickable area and button UI

--- src/ui/common/components/Multistaking/CounterButton.tsx
+++ src/ui/common/components/Multistaking/CounterButton.tsx
@@ -14,37 +14,28 @@ export function CounterButton({
   onAdd,
   alwaysShowCounter = false,
 }: CounterButtonProps) {
+  const isClickable = counter < max;
+
   return (
-    <div className="flex">
+    <div
+      className={twJoin(
+        "flex border border-accent-primary rounded-md bg-primary-highlight overflow-hidden",
+        isClickable && "cursor-pointer",
+      )}
+      onClick={isClickable ? onAdd : undefined}
+    >
       {counter < max && (
-        <div
-          className={twJoin(
-            "w-10 h-10 flex items-center justify-center rounded-md bg-primary-highlight border border-accent-primary cursor-pointer",
-            counter > 0 || alwaysShowCounter ? "rounded-r-none" : "rounded",
-          )}
-          onClick={onAdd}
-        >
+        <div className="w-10 h-10 flex items-center justify-center">
           <AiOutlinePlus size={20} />
         </div>
       )}
       {0 < counter && 1 < max && (
-        <div
-          className={twJoin(
-            "px-2 sm:px-4 h-10 flex items-center border border-accent-primary text-sm sm:text-base",
-            counter === max ? "rounded-md" : "border-l-0 rounded-r-md",
-            "cursor-pointer",
-          )}
-        >
+        <div className="px-2 sm:px-4 h-10 flex items-center text-sm sm:text-base border-l border-accent-primary">
           {counter}/{max}
         </div>
       )}
       {alwaysShowCounter && counter === 0 && (
-        <div
-          className={twJoin(
-            "px-2 sm:px-4 h-10 flex items-center border border-accent-primary rounded-r-md border-l-0 text-sm sm:text-base",
-            "cursor-pointer",
-          )}
-        >
+        <div className="px-2 sm:px-4 h-10 flex items-center text-sm sm:text-base border-l border-accent-primary">
           {counter}/{max}
         </div>
       )}
