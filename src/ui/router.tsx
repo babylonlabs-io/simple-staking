d0c0f1d1c85b0b7d1cfd14a0bdde97dbd9a3d106
David Totraev <totraev@gmail.com>
2025-07-03 19:31:55 +0500

    feat: add baby staking FF

--- src/ui/router.tsx
+++ src/ui/router.tsx
@@ -1,14 +1,16 @@
-import { Navigate, Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 
 import BabyStaking from "./baby";
 import NotFound from "./common/not-found";
 import BTCStaking from "./common/page";
+import FeatureFlagService from "./common/utils/FeatureFlagService";
 
 export const Router = () => (
   <Routes>
-    <Route index element={<Navigate to="btc" replace />} />
-    <Route path="btc" element={<BTCStaking />} />
-    <Route path="baby" element={<BabyStaking />} />
+    <Route index path="/" element={<BTCStaking />} />
+    {FeatureFlagService.IsBabyStakingEnabled && (
+      <Route path="baby" element={<BabyStaking />} />
+    )}
     <Route path="*" element={<NotFound />} />
   </Routes>
 );
