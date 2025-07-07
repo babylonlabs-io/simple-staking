8c82f6ccfa4125d2f7c223a6438abb4e68800379
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-07-07 14:17:15 +0200

    Implement twJoin

--- src/ui/common/components/Menu/MenuButton.tsx
+++ src/ui/common/components/Menu/MenuButton.tsx
@@ -1,5 +1,6 @@
 import type { HTMLAttributes } from "react";
 import { forwardRef } from "react";
+import { twJoin } from "tailwind-merge";
 
 import cogIcon from "@/ui/common/assets/cog.svg";
 
@@ -17,7 +18,10 @@ export const MenuButton = forwardRef<HTMLButtonElement, MenuButtonProps>(
           toggleMenu();
           props.onClick?.(e);
         }}
-        className={`flex items-center justify-center w-10 h-10 p-1 border-secondary-contrast text-secondary-contrast ${className}`}
+        className={twJoin(
+          "flex items-center justify-center w-10 h-10 p-1 border-secondary-contrast text-secondary-contrast",
+          className,
+        )}
       >
         <img src={cogIcon} alt="Settings" className="w-8 h-8" />
       </button>
