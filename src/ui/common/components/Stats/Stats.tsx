e0bdb8759113bfb6429a25985d66bc7bc723e68d
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-07-10 12:19:40 +0100

    Fix background of Stast

--- src/ui/common/components/Stats/Stats.tsx
+++ src/ui/common/components/Stats/Stats.tsx
@@ -30,7 +30,7 @@ export const Stats = memo(() => {
 
   return (
     <Section title="Babylon Stats">
-      <List orientation="adaptive" className="bg-surface" newDesign>
+      <List orientation="adaptive" newDesign>
         <StatItem
           loading={isLoading}
           title={`Total ${coinSymbol} TVL`}
