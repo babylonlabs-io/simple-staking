119f23030c05919cf9b43de52a6e720a10ae034d
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-07-10 12:57:41 +0100

    Fix counter button

--- src/ui/common/components/Multistaking/CounterButton.tsx
+++ src/ui/common/components/Multistaking/CounterButton.tsx
@@ -15,26 +15,24 @@ export function CounterButton({
   alwaysShowCounter = false,
 }: CounterButtonProps) {
   const isClickable = counter < max;
+  const showsCounter =
+    (0 < counter && 1 < max) || (alwaysShowCounter && counter === 0);
 
   return (
     <div
       className={twJoin(
         "flex border border-accent-primary rounded-md bg-primary-highlight overflow-hidden",
         isClickable && "cursor-pointer",
+        !showsCounter && !isClickable && "hidden",
       )}
       onClick={isClickable ? onAdd : undefined}
     >
-      {counter < max && (
+      {isClickable && (
         <div className="w-10 h-10 flex items-center justify-center">
           <AiOutlinePlus size={20} />
         </div>
       )}
-      {0 < counter && 1 < max && (
-        <div className="px-2 sm:px-4 h-10 flex items-center text-sm sm:text-base border-l border-accent-primary">
-          {counter}/{max}
-        </div>
-      )}
-      {alwaysShowCounter && counter === 0 && (
+      {showsCounter && (
         <div className="px-2 sm:px-4 h-10 flex items-center text-sm sm:text-base border-l border-accent-primary">
           {counter}/{max}
         </div>
