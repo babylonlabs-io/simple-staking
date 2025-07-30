eb74d1d4446139b6599718a43d9c606e6cdebdb3
David Totrashvili <8580261+0xDazzer@users.noreply.github.com>
2025-07-30 14:00:59 +0500

    fix(BABY): rename url (#1317)

--- src/infrastructure/babylon.ts
+++ src/infrastructure/babylon.ts
@@ -3,8 +3,7 @@ import { createLCDClient, txs, utils } from "@babylonlabs-io/babylon-proto-ts";
 import config from "./config";
 
 export default {
-  // TODO: rename prop to `lcdUrl` in next release
-  client: await createLCDClient({ lcdClient: config.babylon.lcdUrl }),
+  client: await createLCDClient({ url: config.babylon.rpcUrl }),
   txs,
   utils,
 };
