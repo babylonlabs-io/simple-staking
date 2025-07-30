0d8e331c82060c4e060b7b738e8ae8fd066687e1
David Totrashvili <8580261+0xDazzer@users.noreply.github.com>
2025-07-30 15:41:40 +0500

    feat: claim rewards (#1319)
    * feat: claim rewards
    
    * feat: claim all rewards

--- src/ui/baby/state/RewardState.tsx
+++ src/ui/baby/state/RewardState.tsx
@@ -14,21 +14,21 @@ interface Reward {
 interface RewardState {
   loading: boolean;
   rewards: Reward[];
-  claim: (validatorAddress: string) => Promise<void>;
+  claimAll: () => Promise<void>;
 }
 
 const { StateProvider, useState: useRewardState } =
   createStateUtils<RewardState>({
     loading: false,
     rewards: [],
-    claim: async () => {},
+    claimAll: async () => {},
   });
 
 // TODO: group rewards
 function RewardState({ children }: PropsWithChildren) {
   const [processing, setProcessing] = useState(false);
 
-  const { loading, rewards, claimReward } = useRewardService();
+  const { loading, rewards, claimAllRewards } = useRewardService();
   const { handleError } = useError();
   const logger = useLogger();
 
@@ -49,31 +49,34 @@ function RewardState({ children }: PropsWithChildren) {
     [rewards],
   );
 
-  const claim = useCallback(
-    async (validatorAddress: string) => {
-      try {
-        setProcessing(true);
-        const result = await claimReward(validatorAddress);
-        logger.info("Baby Staking: stake", {
-          txHash: result?.txHash,
-        });
-      } catch (error: any) {
-        handleError({ error });
-        logger.error(error);
-      } finally {
-        setProcessing(false);
-      }
-    },
-    [logger, handleError, claimReward],
+  const totalReward = useMemo(
+    () => rewardList.reduce((total, reward) => total + reward.amount, 0n),
+    [rewardList],
   );
+
+  const claimAll = useCallback(async () => {
+    try {
+      setProcessing(true);
+      const result = await claimAllRewards();
+      logger.info("Baby Staking: stake", {
+        txHash: result?.txHash,
+      });
+    } catch (error: any) {
+      handleError({ error });
+      logger.error(error);
+    } finally {
+      setProcessing(false);
+    }
+  }, [logger, handleError, claimAllRewards]);
 
   const context = useMemo(
     () => ({
       loading: loading || processing,
       rewards: rewardList,
-      claim,
+      totalReward,
+      claimAll,
     }),
-    [loading, processing, rewardList, claim],
+    [loading, processing, rewardList, totalReward, claimAll],
   );
 
   return <StateProvider value={context}>{children}</StateProvider>;
