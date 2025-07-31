d06300fae12ccd4891b66e139514b43f1feeadb6
Govard Barkhatov <gbarkhatov@gmail.com>
2025-07-31 15:06:59 +0300

    chore: can expand in useDelegationStorage

--- src/ui/common/hooks/storage/useDelegationStorage.ts
+++ src/ui/common/hooks/storage/useDelegationStorage.ts
@@ -50,6 +50,7 @@ export function useDelegationStorage(
             unbondingSlashingTxHex: "",
             spendingHeight: 0,
           },
+          canExpand: false,
         }) as DelegationV2,
     );
 
