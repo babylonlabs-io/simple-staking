b115d22252a96d7333bc80a7e63476cdaf1e810d
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-08-06 00:57:17 +0100

    chore(types): flesh out Babylon infrastructure typings and declare sdk module for UI

--- src/types/infrastructure/babylon.d.ts
+++ src/types/infrastructure/babylon.d.ts
@@ -1,12 +1,45 @@
-// TODO: Create and export types from babylon-proto-ts
-import { createBabylonSDK } from "@babylonlabs-io/babylon-proto-ts";
+// Babylon infrastructure typings – non-exhaustive but enough for current UI usage.
+// NOTE: This file augments the types we import from @babylonlabs-io/babylon-proto-ts.
+// If the upstream package publishes richer typings later, this file can be removed.
 
-declare global {
-  namespace Infra {
-    type BabylonSDK = ReturnType<typeof createBabylonSDK>;
-  }
+import type { LCDClient } from "@babylonlabs-io/babylon-proto-ts";
 
-  interface Infra {
-    bitcoin: Infra.BabylonSDK;
+/** Babylon BABY-denominated utility helpers */
+export interface BabylonUtils {
+  /** Convert BABY (1e0) to ubbn (1e6) */
+  babyToUbbn(value: number): bigint;
+  /** Convert ubbn (1e6) to BABY (float) */
+  ubbnToBaby(value: bigint): number;
+}
+
+/** Parameters shared by stake / unstake message builders */
+export interface StakeMsgParams {
+  validatorAddress: string;
+  delegatorAddress: string;
+  /** Amount in ubbn */
+  amount: bigint;
+}
+
+export interface ClaimRewardMsgParams {
+  validatorAddress: string;
+  delegatorAddress: string;
+}
+
+export namespace BabylonTxs {
+  namespace baby {
+    function createStakeMsg(params: StakeMsgParams): unknown;
+    function createUnstakeMsg(params: StakeMsgParams): unknown;
+    function createClaimRewardMsg(params: ClaimRewardMsgParams): unknown;
   }
+}
+
+export interface BabylonSDK {
+  client: LCDClient;
+  txs: typeof BabylonTxs;
+  utils: BabylonUtils;
+}
+
+declare module "@/infrastructure/babylon" {
+  const sdk: BabylonSDK;
+  export default sdk;
 }
