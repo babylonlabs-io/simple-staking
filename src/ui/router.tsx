766a70d00a01ed7b68da3352503dff0b55bcd7e2
Jony Bursztyn <jonathan@bursztyn.io>
2025-08-12 10:03:15 +0100

    feat: restores the navbar, improvements to tabs (#1380)

--- src/ui/router.tsx
+++ src/ui/router.tsx
@@ -1,9 +1,6 @@
 import { Navigate, Route, Routes } from "react-router";
 
 import BabyLayout from "./baby/layout";
-import BabyActivities from "./baby/widgets/Activities";
-import BabyRewards from "./baby/widgets/Rewards";
-import BabyStakingForm from "./baby/widgets/StakingForm";
 import Layout from "./common/layout";
 import NotFound from "./common/not-found";
 import BTCStaking from "./common/page";
@@ -16,12 +13,7 @@ export const Router = () => {
         <Route index element={<Navigate to="btc" replace />} />
         <Route path="btc" element={<BTCStaking />} />
         {FF.IsBabyStakingEnabled && (
-          <Route path="baby" element={<BabyLayout />}>
-            <Route index element={<Navigate to="staking" replace />} />
-            <Route path="staking" element={<BabyStakingForm />} />
-            <Route path="rewards" element={<BabyRewards />} />
-            <Route path="activities" element={<BabyActivities />} />
-          </Route>
+          <Route path="baby" element={<BabyLayout />} />
         )}
       </Route>
       <Route path="*" element={<NotFound />} />
