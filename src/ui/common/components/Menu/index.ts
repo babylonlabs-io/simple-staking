bbe92e1faefbb37d3ebed1c4c1973732534e745c
jeremy-babylonchain <jeremy.tsang@babylonchain.io>
2025-08-17 17:57:58 +0800

    feat: implement wallet menu

--- src/ui/common/components/Menu/index.ts
+++ src/ui/common/components/Menu/index.ts
@@ -1,3 +1,1 @@
 export * from "./SettingMenu";
-
-export * from "./WalletMenu";
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,10 @@ const coreUIConfig = require("@babylonlabs-io/core-ui/tailwind");
 
 const config: Config = {
   presets: [coreUIConfig],
-  content: ["./src/ui/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./src/ui/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@babylonlabs-io/core-ui/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: "class",
   theme: {
     screens: screenBreakPoints,
