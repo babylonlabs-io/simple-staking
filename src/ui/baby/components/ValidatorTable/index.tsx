497aae7fa7f21bd3288597a6e2a4cf679e4b9da9
Necip Baha Sağıroğlu <sagiroglun22@itu.edu.tr>
2025-08-29 15:29:27 +0300

    feat: show total stake of validator in validator selection #1487

--- src/ui/baby/components/ValidatorTable/index.tsx
+++ src/ui/baby/components/ValidatorTable/index.tsx
@@ -61,7 +61,7 @@ export const ValidatorTable = ({
   );
 
   return (
-    <ResponsiveDialog open={open} onClose={onClose}>
+    <ResponsiveDialog open={open} onClose={onClose} className="w-[52rem]">
       <DialogHeader
         title="Select Validator"
         onClose={onClose}
