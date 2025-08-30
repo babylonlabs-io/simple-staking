8f9357cb55b97dba006b09c1d7f190390c9c0e6b
Jonathan Bursztyn <jonathan@bursztyn.io>
2025-08-30 02:17:08 +0200

    remove preview modal from BABY staking

--- src/ui/baby/widgets/StakingModal/index.tsx
+++ src/ui/baby/widgets/StakingModal/index.tsx
@@ -1,22 +1,60 @@
-import { useFormContext } from "@babylonlabs-io/core-ui";
+import { PreviewModal, useFormContext } from "@babylonlabs-io/core-ui";
+
+import babylon from "@/infrastructure/babylon";
+import { ValidatorAvatar } from "@/ui/baby/components/ValidatorAvatar";
+import { getNetworkConfigBBN } from "@/ui/common/config/network/bbn";
+import { maxDecimals } from "@/ui/common/utils/maxDecimals";
 
 import { LoadingModal } from "../../components/LoadingModal";
-import { PreviewModal } from "../../components/PreviewModal";
 import { SuccessModal } from "../../components/SuccessModal";
 import { useStakingState } from "../../state/StakingState";
 
 export function StakingModal() {
   const { step, closePreview, submitForm } = useStakingState();
   const { reset } = useFormContext();
+  const { coinSymbol } = getNetworkConfigBBN();
 
   return (
     <>
       {step.name === "preview" && step.data && (
         <PreviewModal
           open
-          data={step.data}
+          processing={false}
+          bsns={[
+            {
+              icon: (
+                <ValidatorAvatar
+                  url={step.data.validator.url}
+                  name={step.data.validator.name}
+                  variant="rounded"
+                  size="tiny"
+                />
+              ),
+              name: step.data.validator.name,
+            },
+          ]}
+          finalityProviders={[]}
+          details={{
+            stakeAmount: `${maxDecimals(
+              babylon.utils.ubbnToBaby(BigInt(step.data.amount)),
+              6,
+            )} ${coinSymbol}`,
+            feeRate: "",
+            transactionFees: `${maxDecimals(
+              babylon.utils.ubbnToBaby(BigInt(step.data.feeAmount)),
+              6,
+            )} ${coinSymbol}`,
+            term: { blocks: "", duration: "" },
+            unbonding: "",
+            unbondingFee: "",
+          }}
+          visibleFields={["Stake Amount", "Transaction Fees"]}
+          warnings={[
+            "The staking transaction may take up to one (1) hour to process. Funds will not be deducted instantly; a sufficient available balance must be maintained until the transaction is confirmed and the deduction is finalized.",
+          ]}
           onClose={closePreview}
-          onSubmit={submitForm}
+          onProceed={submitForm}
+          proceedLabel="Stake"
         />
       )}
       {step.name === "signing" && (
